0c5e41f6b148a617c2e871a34726b8c89f4afd1d
Danyang Zhuo <danyangzhuo@gmail.com>
2019-12-16 23:42:48 +0000

    a script to run mpi reduce

---
+++ mpi_reduce.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [ -z "$1" ]; then echo "ERROR: input size required"; exit; fi
+
+worker_pubips=$(ray get-worker-ips ~/ray_bootstrap_config.yaml)
+
+master=$(ifconfig | grep 'inet.*broadcast' | awk '{print $2}')
+
+slaves=()
+for s in $worker_pubips; do slaves+=($(ssh -o StrictHostKeyChecking=no $s ifconfig | grep 'inet.*broadcast' | awk '{print $2}')); done
+
+echo $master
+echo ${slaves[@]}
+
+all_nodes=(${slaves[@]})
+all_nodes+=($master)
+echo ${all_nodes[@]}, ${#all_nodes[@]}
+
+all_hosts=$(echo ${all_nodes[@]} | sed 's/ /,/g')
+
+mpirun --map-by ppr:1:node -hosts $all_hosts /home/ubuntu/efs/mpitutorial/tutorials/mpi-reduce-and-allreduce/code/reduce_avg $[$1/4]
