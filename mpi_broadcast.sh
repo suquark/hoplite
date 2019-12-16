3a85e39b2bb096af777ae7c641c356364e0d5082
Danyang Zhuo <danyangzhuo@gmail.com>
2019-12-16 18:53:23 +0000

    disable strict host checking when ssh

--- mpi_broadcast.sh
+++ mpi_broadcast.sh
@@ -6,7 +6,7 @@ worker_pubips=$(ray get-worker-ips ~/ray_bootstrap_config.yaml)
 master=$(ifconfig | grep 'inet.*broadcast' | awk '{print $2}')
 
 slaves=()
-for s in $worker_pubips; do slaves+=($(ssh $s ifconfig | grep 'inet.*broadcast' | awk '{print $2}')); done
+for s in $worker_pubips; do slaves+=($(ssh -o StrictHostKeyChecking=no $s ifconfig | grep 'inet.*broadcast' | awk '{print $2}')); done
 
 echo $master
 echo ${slaves[@]}
--- speed_test.sh
+++ speed_test.sh
@@ -5,7 +5,7 @@ worker_pubips=$(ray get-worker-ips ~/ray_bootstrap_config.yaml)
 master=$(ifconfig | grep 'inet.*broadcast' | awk '{print $2}')
 
 slaves=()
-for s in $worker_pubips; do slaves+=($(ssh $s ifconfig | grep 'inet.*broadcast' | awk '{print $2}')); done
+for s in $worker_pubips; do slaves+=($(ssh -o StrictHostKeyChecking=no $s ifconfig | grep 'inet.*broadcast' | awk '{print $2}')); done
 
 all_nodes=(${slaves[@]})
 all_nodes+=($master)
@@ -14,7 +14,7 @@ echo ${all_nodes[@]}, ${#all_nodes[@]}
 # start iperf server
 for s in ${all_nodes[@]}
 do
-	ssh $s pkill iperf
+	ssh -o StrictHostKeyChecking=no $s pkill iperf
 	ssh $s iperf -s &> /dev/null &
 done
 
