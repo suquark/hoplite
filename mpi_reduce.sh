46c60f9cd5618903697b047440a6abbc376bf2a1
Danyang Zhuo <danyangzhuo@gmail.com>
2019-12-17 00:11:24 +0000

    MPI reduce

--- mpi_reduce.sh
+++ mpi_reduce.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 if [ -z "$1" ]; then echo "ERROR: input size required"; exit; fi
 
+cp mpi/reduce.c /home/ubuntu/efs/mpitutorial/tutorials/mpi-reduce-and-allreduce/code/reduce_avg.c
+pushd /home/ubuntu/efs/mpitutorial/tutorials/mpi-reduce-and-allreduce/code/
+make
+popd
+
 worker_pubips=$(ray get-worker-ips ~/ray_bootstrap_config.yaml)
 
 master=$(ifconfig | grep 'inet.*broadcast' | awk '{print $2}')
 
 slaves=()
 for s in $worker_pubips; do slaves+=($(ssh -o StrictHostKeyChecking=no $s ifconfig | grep 'inet.*broadcast' | awk '{print $2}')); done
-
-echo $master
-echo ${slaves[@]}
 
 all_nodes=(${slaves[@]})
 all_nodes+=($master)
