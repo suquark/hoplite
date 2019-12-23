2d999bd83601e58d79382bec647682ec1cc91b3c
Siyuan <suquark@gmail.com>
2019-12-23 00:01:43 -0800

    fix random seed

--- reduce_test.cc
+++ reduce_test.cc
@@ -13,17 +13,18 @@
 
 using namespace plasma;
 
-float get_uniform_random_float() {
-  std::random_device rd;
-  std::mt19937 gen(rd());
-  std::uniform_real_distribution<> dis(0, 1);
-  return dis(gen);
+float get_uniform_random_float(const std::string &seed_str) {
+  std::seed_seq seed(seed_str.begin(), seed_str.end());
+  std::default_random_engine eng{seed};
+  std::uniform_real_distribution<float> dis(0, 1);
+  return dis(eng);
 }
 
-std::unique_ptr<std::vector<float>> get_random_float_buffer(size_t size) {
+std::unique_ptr<std::vector<float>>
+get_random_float_buffer(size_t size, const std::string &seed_str) {
   std::unique_ptr<std::vector<float>> retval;
   auto buf = new std::vector<float>(size);
-  float random_number = get_uniform_random_float();
+  float random_number = get_uniform_random_float(seed_str);
   for (int i = 0; i < size; i++) {
     (*buf)[i] = i * random_number;
   }
@@ -33,6 +34,7 @@ std::unique_ptr<std::vector<float>> get_random_float_buffer(size_t size) {
 
 void test_server(DistributedObjectStore &store, int object_size,
                  const std::vector<ObjectID> &object_ids) {
+  DCHECK(object_size % sizeof(float) == 0);
   float *buffer;
   size_t size;
 
@@ -44,14 +46,16 @@ void test_server(DistributedObjectStore &store, int object_size,
   unsigned long crc = crc32(0L, Z_NULL, 0);
   crc = crc32(crc, (const unsigned char *)buffer, size);
   LOG(INFO) << "Object is reduced using " << duration.count()
-            << " seconds. CRC32 = " << crc;
+            << " seconds. CRC32 = " << crc << "; Results: [" << buffer[0]
+            << ", " << buffer[1] << ", " << buffer[2] << ", ... ,"
+            << buffer[object_size / sizeof(float)] << "]";
 }
 
 void test_client(DistributedObjectStore &store, int object_size,
                  ObjectID object_id) {
-  DCHECK(object_size % 4 == 0);
+  DCHECK(object_size % sizeof(float) == 0);
   std::unique_ptr<std::vector<float>> buffer =
-      get_random_float_buffer(object_size / sizeof(float));
+      get_random_float_buffer(object_size / sizeof(float), object_id.hex());
   store.Put(buffer->data(), object_size, object_id);
   LOG(INFO) << "Object is created! object_id = " << object_id.hex()
             << ", size = " << object_size;
