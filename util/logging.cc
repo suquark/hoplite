bde70c2073b14077278eef253ec8a58b2ba05e8a
Zhuohan Li <zhuohan123@vip.qq.com>
2020-01-03 15:38:09 -0800

    lint

--- util/logging.cc
+++ util/logging.cc
@@ -135,7 +135,7 @@ LogFunc::LogFunc(const std::string& file_name, int line_number,
     function_name_(function_name), message_(message) {
   std::stringstream hashstampstream;
   hashstampstream << std::chrono::high_resolution_clock::now().time_since_epoch().count();
-  hashstamp_ = hashstampstream.str()
+  hashstamp_ = hashstampstream.str();
   if (ray::RayLog::IsLevelEnabled(ray::RayLogLevel::INFO))
     ::ray::RayLog(file_name_.c_str(), line_number_, function_name_.c_str(), ray::RayLogLevel::INFO) << "[TIMELINE] [" << hashstamp_ << "] [BEGIN] " << message_;
 }
