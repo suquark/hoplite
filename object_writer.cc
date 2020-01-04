bf22c9e845b9593da138536cb0497900fd24029b
Zhuohan Li <zhuohan123@vip.qq.com>
2020-01-03 17:29:14 -0800

    add timeline to tcpserver

--- object_writer.cc
+++ object_writer.cc
@@ -119,7 +119,7 @@ void TCPServer::worker_loop() {
 void TCPServer::receive_and_reduce_object(
     int conn_fd, const ObjectID &reduction_id,
     const std::vector<ObjectID> &object_ids, bool is_endpoint) {
-  TIMELINE(std::string("TCPServer::receive_and_reduce_object() ") + object_id.hex() + " " + to_string(object_size));
+  TIMELINE(std::string("TCPServer::receive_and_reduce_objectt() ") + object_id.hex() + " " + std::to_string(object_size));
 
   // The endpoint can have no objects to reduce.
   DCHECK(object_ids.size() > 0 || is_endpoint)
@@ -163,7 +163,7 @@ void TCPServer::receive_and_reduce_object(
 
 void TCPServer::receive_object(int conn_fd, const ObjectID &object_id,
                                int64_t object_size) {
-  TIMELINE(std::string("TCPServer::receive_object() ") + object_id.hex() + " " + to_string(object_size));
+  TIMELINE(std::string("TCPServer::receive_object() ") + object_id.hex() + " " + std::to_string(object_size));
   LOG(DEBUG) << "start receiving object " << object_id.hex()
              << ", size = " << object_size;
 
