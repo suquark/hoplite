1b974b7191d0576e04ba4698335d355f3ac36257
Danyang Zhuo <danyangzhuo@gmail.com>
2020-01-06 00:53:56 -0800

    Parse results from auto tests (#29)
    * parse multicast result
    
    * parse reduce time

--- auto_test.sh
+++ auto_test.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-if [ "$#" -lt 1 ]; then echo "ERROR: number of nodes"; exit; fi
-if [ "$#" -gt 2 ]; then echo "ERROR: too many arguments: $#"; exit; fi
-
-num_tests=10
+if [ "$#" -lt 1 ]; then echo "ERROR: number of nodes & number of tests"; exit; fi
+if [ "$#" -gt 3 ]; then echo "ERROR: too many arguments: $#"; exit; fi
 
 # multicast
 for i in `seq 6 10`;
 do
-	for test_index in `seq 1 $num_tests`;
+	for test_index in `seq 1 $2`;
 	do
 		obj_size=$((8**$i))
 		./multicast_test.sh $1 $obj_size
@@ -17,7 +15,7 @@ done
 # reduce
 for i in `seq 6 10`;
 do
-	for test_index in `seq 1 $num_tests`;
+	for test_index in `seq 1 $2`;
 	do
 		obj_size=$((8**$i))
 		./reduce_test.sh $1 $obj_size
---
+++ script/parse_result.py
@@ -0,0 +1,97 @@
+import os
+import sys
+
+def parse_multicast(folder_path):
+    last_retrieval_time = 0
+    files = os.listdir(folder_path)
+    for filename in files:
+        if 'server' in filename:
+            continue
+        try:
+            f = open(os.path.join(folder_path, filename))
+            for line in f.readlines():
+                if 'Object is retrieved' in line:
+                    tmp = line.split('Object is retrieved using')[1]
+                    tmp = tmp.split('seconds')[0]
+                    retrival_time = float(tmp)
+                    if retrival_time > last_retrieval_time:
+                        last_retrieval_time = retrival_time
+
+            f.close()
+        except:
+            print (filename)
+    return last_retrieval_time
+
+def parse_reduce(folder_path):
+    files = os.listdir(folder_path)
+    for filename in files:
+        if 'client' in filename:
+            continue
+        try:
+            f = open(os.path.join(folder_path, filename))
+            for line in f.readlines():
+                if 'Object is reduced' in line:
+                    tmp = line.split('Object is reduced using')[1]
+                    tmp = tmp.split('seconds')[0]
+                    reduce_time = float(tmp)
+            f.close()
+        except:
+            print (filename)
+
+    return reduce_time
+
+def parse_file(task_name, log_dir, foldername):
+    path = os.path.join(log_dir, foldername)
+
+    if task_name == 'multicast':
+        return parse_multicast(path)
+
+    if task_name == 'reduce':
+        return parse_reduce(path)
+
+    assert (False)
+
+def main(log_dir):
+    files = os.listdir(log_dir)
+
+    tasks = {}
+
+    for filename in files:
+        splited = filename.split('-');
+        if len(splited) != 5:
+            exit(-1)
+        task_name = splited[2]
+        number_of_nodes = splited[3]
+        object_size = splited[4]
+        
+        task = task_name + '-' + number_of_nodes + '-' + object_size
+
+        if task not in tasks:
+            tasks[task] = []
+
+        tasks[task].append(filename)
+
+    results = {}
+    
+    for task in tasks:
+        result = 0
+        for foldername in tasks[task]:
+            result += parse_file(task.split('-')[0], log_dir, foldername);
+        result /= len(tasks[task])
+
+        results[task] = result
+
+    task_list = []
+    for task in results:
+        task_list.append(task)
+
+    task_list = sorted(task_list, reverse=True)
+
+    for task in task_list:
+        print (task, results[task])
+
+
+if __name__ == "__main__":
+    assert len(sys.argv) == 2, "Usage: python parse_result.py LOG_DIR"
+    log_dir = sys.argv[1]
+    main(log_dir)
