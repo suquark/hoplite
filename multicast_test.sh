f21b583bda56463d1f0503a2c8ebcbe4693c5cd1
Danyang Zhuo <danyangzhuo@gmail.com>
2020-01-05 16:04:41 -0800

    Allow variable number of nodes in multicast and reduce scripts (#27)
    * support variable number of nodes in multicast script
    
    * allow variable number of nodes in reduce script

--- multicast_test.sh
+++ multicast_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ "$#" -lt 1 ]; then echo "ERROR: input size required"; exit; fi
-if [ "$#" -gt 3 ]; then echo "ERROR: too many arguments: $#"; exit; fi
+if [ "$#" -lt 1 ]; then echo "ERROR: number of nodes & input size required"; exit; fi
+if [ "$#" -gt 4 ]; then echo "ERROR: too many arguments: $#"; exit; fi
 
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM SIGHUP EXIT
 
@@ -14,30 +14,30 @@ sleep 2
 
 working_dir=$(dirname $(realpath -s $0))
 
-if [ "$#" -eq 1 ]; then
+if [ "$#" -eq 2 ]; then
 	redis-server redis.conf &> /dev/null &  # port = 6380
 	sleep 2
 	worker_pubips=$(ray get-worker-ips ~/ray_bootstrap_config.yaml)
 	slaves=()
 	for s in $worker_pubips; do slaves+=($(ssh -o StrictHostKeyChecking=no $s ifconfig | grep 'inet.*broadcast' | awk '{print $2}')); done
-	# slaves=(${slaves[@]:0:1})
+	slaves=(${slaves[@]:0:$(($1-1))})
 	echo "[Putting Object] master: $my_address; slaves: ${slaves[@]}"
 	log_dir=$working_dir/log/$(date +"%Y%m%d-%H%M%S")-multicast
 	mkdir -p $log_dir
 
 	## multicast
-	($working_dir/multicast_test $my_address $my_address s $1 2>&1 | tee $log_dir/$my_address.server.log) &
+	($working_dir/multicast_test $my_address $my_address s $2 2>&1 | tee $log_dir/$my_address.server.log) &
 	sleep 2
 
 	for slave in ${slaves[@]}
 	do
-		ssh -t -t $slave "$(realpath -s $0) $my_address 8c97b7d89adb8bd86c9fa562704ce40ef645627a $log_dir" &
+		ssh -t -t $slave "$(realpath -s $0) $1 $my_address 8c97b7d89adb8bd86c9fa562704ce40ef645627a $log_dir" &
 	done
 else
 	# sudo fuser -km /tmp/multicast_plasma
-	echo "[Getting Object] redis_address: $1 object_id: $2 my_address: $my_address"
+	echo "[Getting Object] redis_address: $2 object_id: $3 my_address: $my_address"
 	## multicast
-	($working_dir/multicast_test $1 $my_address c $2 2>&1 | tee $3/$my_address.client.log) &
+	($working_dir/multicast_test $2 $my_address c $3 2>&1 | tee $4/$my_address.client.log) &
 fi
 
 sleep 36
--- reduce_test.sh
+++ reduce_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ "$#" -lt 1 ]; then echo "ERROR: input size required"; exit; fi
-if [ "$#" -gt 4 ]; then echo "ERROR: too many arguments: $#"; exit; fi
+if [ "$#" -lt 2 ]; then echo "ERROR: number of nodes & input size required"; exit; fi
+if [ "$#" -gt 5 ]; then echo "ERROR: too many arguments: $#"; exit; fi
 
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM SIGHUP EXIT
 
@@ -15,14 +15,14 @@ plasma-store-server -m 4000000000 -s /tmp/multicast_plasma &> /dev/null &
 
 working_dir=$(dirname $(realpath -s $0))
 
-if [ "$#" -eq 1 ]; then
+if [ "$#" -eq 2 ]; then
 	redis-server redis.conf &> /dev/null &  # port = 6380
 	sleep 2
 
 	worker_pubips=$(ray get-worker-ips ~/ray_bootstrap_config.yaml)
 	slaves=()
 	for s in $worker_pubips; do slaves+=($(ssh -o StrictHostKeyChecking=no $s ifconfig | grep 'inet.*broadcast' | awk '{print $2}')); done
-	# slaves=(${slaves[@]:0:1})
+        slaves=(${slaves[@]:0:$(($1-1))})
 	echo "[Reducing Object] master: $my_address; slaves: ${slaves[@]}"
 
 	object_ids=()
@@ -30,17 +30,17 @@ if [ "$#" -eq 1 ]; then
 	log_dir=$working_dir/log/$(date +"%Y%m%d-%H%M%S")-reduce
 	mkdir -p $log_dir
 
-	($working_dir/reduce_test $my_address $my_address s $1 ${object_ids[@]} 2>&1 | tee $log_dir/$my_address.server.log) &
+	($working_dir/reduce_test $my_address $my_address s $2 ${object_ids[@]} 2>&1 | tee $log_dir/$my_address.server.log) &
 	# sleep 2
 
 	for index in ${!slaves[@]}
 	do
-		ssh -t -t ${slaves[$index]} "$(realpath -s $0) $my_address $1 ${object_ids[$index]} $log_dir" &
+		ssh -t -t ${slaves[$index]} "$(realpath -s $0) $1 $my_address $2 ${object_ids[$index]} $log_dir" &
 	done
 else
 	# sudo fuser -km /tmp/multicast_plasma
-	echo "[Putting Object] redis_address: $1 object_size: $2 objectid: $3 my_address: $my_address"
-	$working_dir/reduce_test $1 $my_address c $2 $3 2>&1 | tee $4/$my_address.client.log
+	echo "[Putting Object] redis_address: $2 object_size: $3 objectid: $4 my_address: $my_address"
+	$working_dir/reduce_test $2 $my_address c $3 $4 2>&1 | tee $5/$my_address.client.log
 fi
 
 sleep 36
