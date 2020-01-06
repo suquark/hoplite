624b72c38ebee0be044ff36715d3a8e99100d201
Siyuan <suquark@gmail.com>
2020-01-05 23:30:53 -0800

    fix scripts

--- all_reduce_test.sh
+++ all_reduce_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ "$#" -lt 1 ]; then echo "ERROR: input size required"; exit; fi
-if [ "$#" -gt 4 ]; then echo "ERROR: too many arguments: $#"; exit; fi
+if [ "$#" -lt 2 ]; then echo "ERROR: number of nodes & input size required"; exit; fi
+if [ "$#" -gt 5 ]; then echo "ERROR: too many arguments: $#"; exit; fi
 
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM SIGHUP EXIT
 
@@ -15,32 +15,30 @@ plasma-store-server -m 4000000000 -s /tmp/multicast_plasma &> /dev/null &
 
 working_dir=$(dirname $(realpath -s $0))
 
-if [ "$#" -eq 1 ]; then
+if [ "$#" -eq 2 ]; then
 	redis-server redis.conf &> /dev/null &  # port = 6380
 	sleep 2
 
 	worker_pubips=$(ray get-worker-ips ~/ray_bootstrap_config.yaml)
 	slaves=()
 	for s in $worker_pubips; do slaves+=($(ssh -o StrictHostKeyChecking=no $s ifconfig | grep 'inet.*broadcast' | awk '{print $2}')); done
-	# slaves=(${slaves[@]:0:1})
+    slaves=(${slaves[@]:0:$(($1-1))})
 	echo "[Reducing Object] master: $my_address; slaves: ${slaves[@]}"
 
-	object_ids=()
-	for oid in $(seq -f "%040g" 1 ${#slaves[@]}); do object_ids+=($oid); done
-	log_dir=$working_dir/log/$(date +"%Y%m%d-%H%M%S")-allreduce
+	log_dir=$working_dir/log/$(date +"%Y%m%d-%H%M%S")-allreduce-$1-$2
 	mkdir -p $log_dir
 
-	($working_dir/all_reduce_test $my_address $my_address s $1 ${object_ids[@]} 2>&1 | tee $log_dir/$my_address.server.log) &
+	($working_dir/reduce_test $my_address $my_address $1 0 $2 2>&1 | tee $log_dir/$my_address.server.log) &
 	# sleep 2
 
 	for index in ${!slaves[@]}
 	do
-		ssh -t -t ${slaves[$index]} "$(realpath -s $0) $my_address $1 ${object_ids[$index]} $log_dir" &
+		ssh -t -t ${slaves[$index]} "$(realpath -s $0) $1 $index $my_address $2 $log_dir" &
 	done
 else
 	# sudo fuser -km /tmp/multicast_plasma
-	echo "[Putting Object] redis_address: $1 object_size: $2 objectid: $3 my_address: $my_address"
-	$working_dir/all_reduce_test $1 $my_address c $2 $3 2>&1 | tee $4/$my_address.client.log
+	echo "[Putting Object] world_size: $1 rank: $2 redis_address: $3 object_size: $4 my_address: $my_address"
+	$working_dir/reduce_test $3 $my_address $1 $2 $4 2>&1 | tee $5/$my_address.client.log
 fi
 
-sleep 36
+sleep 30
--- reduce_test.sh
+++ reduce_test.sh
@@ -22,25 +22,23 @@ if [ "$#" -eq 2 ]; then
 	worker_pubips=$(ray get-worker-ips ~/ray_bootstrap_config.yaml)
 	slaves=()
 	for s in $worker_pubips; do slaves+=($(ssh -o StrictHostKeyChecking=no $s ifconfig | grep 'inet.*broadcast' | awk '{print $2}')); done
-        slaves=(${slaves[@]:0:$(($1-1))})
+    slaves=(${slaves[@]:0:$(($1-1))})
 	echo "[Reducing Object] master: $my_address; slaves: ${slaves[@]}"
 
-	object_ids=()
-	for oid in $(seq -f "%040g" 1 ${#slaves[@]}); do object_ids+=($oid); done
 	log_dir=$working_dir/log/$(date +"%Y%m%d-%H%M%S")-reduce-$1-$2
 	mkdir -p $log_dir
 
-	($working_dir/reduce_test $my_address $my_address s $2 ${object_ids[@]} 2>&1 | tee $log_dir/$my_address.server.log) &
+	($working_dir/reduce_test $my_address $my_address $1 0 $2 2>&1 | tee $log_dir/$my_address.server.log) &
 	# sleep 2
 
 	for index in ${!slaves[@]}
 	do
-		ssh -t -t ${slaves[$index]} "$(realpath -s $0) $1 $my_address $2 ${object_ids[$index]} $log_dir" &
+		ssh -t -t ${slaves[$index]} "$(realpath -s $0) $1 $index $my_address $2 $log_dir" &
 	done
 else
 	# sudo fuser -km /tmp/multicast_plasma
-	echo "[Putting Object] redis_address: $2 object_size: $3 objectid: $4 my_address: $my_address"
-	$working_dir/reduce_test $2 $my_address c $3 $4 2>&1 | tee $5/$my_address.client.log
+	echo "[Putting Object] world_size: $1 rank: $2 redis_address: $3 object_size: $4 my_address: $my_address"
+	$working_dir/reduce_test $3 $my_address $1 $2 $4 2>&1 | tee $5/$my_address.client.log
 fi
 
-sleep 36
+sleep 30
