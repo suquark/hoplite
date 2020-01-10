bc6e5e659eefb93b2b1ed598ac224a528e947d40
Zhuohan Li <zhuohan123@vip.qq.com>
2020-01-09 17:10:48 -0800

    debug

--- src/object_store_state.cc
+++ src/object_store_state.cc
@@ -36,7 +36,7 @@ ObjectStoreState::create_reduction_stream(const plasma::ObjectID &reduction_id,
 std::shared_ptr<ReductionStream>
 ObjectStoreState::get_reduction_stream(const plasma::ObjectID &reduction_id) {
   std::unique_lock<std::mutex> l(reduction_stream_mutex_);
-  reduction_stream_cv_.wait(l, [this](){return reduction_stream_.find(reduction_id) != reduction_stream_.end();});
+  reduction_stream_cv_.wait(l, [this, &reduction_id](){return reduction_stream_.find(reduction_id) != reduction_stream_.end();});
   return reduction_stream_[reduction_id];
 }
 
