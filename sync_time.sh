9bcfd7a1e5a96007e12f656b33a15d3fd393c8f4
Danyang Zhuo <danyangzhuo@gmail.com>
2020-01-12 00:18:42 +0000

    script to sync time

---
+++ sync_time.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo apt install -y chrony
+sudo sed -i 's/^# information about usuable directives./server 169.254.169.123 prefer iburst minpoll 4 maxpoll 4\n/g' /etc/chrony/chrony.conf
+sudo /etc/init.d/chrony restart
+
+working_dir=$(dirname $(realpath -s $0))
+
+if [ "$#" -eq 0 ]; then
+    worker_pubips=$(ray get-worker-ips ~/ray_bootstrap_config.yaml)
+    slaves=()
+    for s in $worker_pubips; do slaves+=($(ssh -o StrictHostKeyChecking=no $s ifconfig | grep 'inet.*broadcast' | awk '{print $2}')); done
+
+    for index in ${!slaves[@]}
+    do
+        ssh -t -t ${slaves[$index]} "$(realpath -s $0) 0" 
+    done
+fi
