da33c383535c7e0e11ed6ba4bb4cb0c0e9f9be0b
Siyuan <suquark@gmail.com>
2020-01-12 15:53:36 -0800

    fix

--- python/multicast_test.py
+++ python/multicast_test.py
@@ -5,23 +5,23 @@ import ray
 
 import py_distributed_object_store as store_lib
 
-from utils import add_arguments, get_my_address, create_store_using_args
+import utils
 
-parser = argparse.ArgumentParser(description='The basic test example')
-add_arguments(parser)
+parser = argparse.ArgumentParser(description='broadcast test')
+utils.add_arguments(parser)
 parser.add_argument('--world-size', type=int, required=True,
                     help='Size of the collective processing group')
 parser.add_argument('--object-size', type=int, required=True,
                     help='The size of the object')
 
 args = parser.parse_args()
-
-store = create_store_using_args(args)
-object_id = store_lib.ObjectID(b'\0' * 20)
+args_dict = utils.extract_dict_from_args(args)
 
 
 @ray.remote(resources={'node': 1}, max_calls=1)
 def multicast(world_rank, object_size):
+    store = utils.create_store_using_dict(args_dict)
+    object_id = store_lib.ObjectID(b'\0' * 20)
     if world_rank == 0:
         array = np.random.randint(2**30, size=object_size//4, dtype=np.int32)
         buffer = store_lib.Buffer.from_numpy(array)
@@ -30,7 +30,8 @@ def multicast(world_rank, object_size):
         buffer = store.get(buffer, object_id)
 
 
-notification_p = subprocess.Popen(['../notification', get_my_address()])
+notification_p = subprocess.Popen(['../notification', utils.get_my_address()])
+utils.register_cleanup([notification_p])
 
 ray.init(address='auto')
 
@@ -41,5 +42,3 @@ for rank in range(args.world_size):
     tasks.append(task_id)
 
 ray.get(tasks)
-
-notification_p.terminate()
--- python/utils.py
+++ python/utils.py
@@ -1,3 +1,6 @@
+import atexit
+
+
 def get_my_address():
     import socket
     return socket.gethostbyname(socket.gethostname())
@@ -20,12 +23,35 @@ def add_arguments(parser):
                         help='The path of the unix domain socket')
 
 
-def create_store_using_args(args):
+def extract_dict_from_args(args):
+    return {
+        'redis_address': args.redis_address.encode(),
+        'redis_port': args.redis_port,
+        'notification_port': args.notification_port,
+        'notification_listening_port': args.notification_listening_port,
+        'plasma_socket': args.plasma_socket.encode(),
+        'my_address': get_my_address().encode(),
+        'object_writer_port': args.object_writer_port,
+        'grpc_port': args.grpc_port,
+    }
+
+
+def create_store_using_dict(args_dict):
     import py_distributed_object_store as store_lib
     store = store_lib.DistributedObjectStore(
-        args.redis_address.encode(), args.redis_port,
-        args.notification_port, args.notification_listening_port,
-        args.plasma_socket.encode(),
-        get_my_address().encode(), args.object_writer_port,
-        args.grpc_port)
+        args_dict['redis_address'],
+        args_dict['redis_port'],
+        args_dict['notification_port'],
+        args_dict['notification_listening_port'],
+        args_dict['plasma_socket'],
+        args_dict['my_address'],
+        args_dict['object_writer_port'],
+        args_dict['grpc_port'])
     return store
+
+
+def register_cleanup(processes):
+    def _cleanup_processes():
+        for p in processes:
+            p.terminate()
+    atexit.register(_cleanup_processes)
