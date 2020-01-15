b623ae04bcdcebc862ae36ea0d8d121c982fd30c
Zhuohan Li <zhuohan123@vip.qq.com>
2020-01-14 17:52:40 -0800

    debug

--- notification_test.sh
+++ notification_test.sh
@@ -8,7 +8,8 @@ sudo fuser -k 50055/tcp -s &> /dev/null
 my_address=$(ifconfig | grep 'inet.*broadcast' | awk '{print $2}')
 working_dir=$(dirname $(realpath -s $0))
 
-pkill notification
+pkill '^notification$'
+pkill '^notification_server_test$'
 sleep 2
 ./notification $my_address &
 sleep 2
