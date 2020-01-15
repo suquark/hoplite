0d9e0b9685da811c73787e52dfe2c1f978815851
Zhuohan Li <zhuohan123@vip.qq.com>
2020-01-14 17:23:04 -0800

    add test script

---
+++ notification_test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM SIGHUP EXIT
+
+sudo fuser -k 6666/tcp -s &> /dev/null
+sudo fuser -k 50055/tcp -s &> /dev/null
+
+## setup
+my_address=$(ifconfig | grep 'inet.*broadcast' | awk '{print $2}')
+working_dir=$(dirname $(realpath -s $0))
+
+pkill notification
+sleep 2
+./notification $my_address &
+sleep 2
+./notification_server_test $my_address
