5067be1813ba0d2d234a8914e1b32f18578b33fd
Siyuan <suquark@gmail.com>
2020-01-15 23:37:02 -0800

    fix auto test

--- auto_test.sh
+++ auto_test.sh
@@ -4,42 +4,17 @@ if [ "$#" -gt 2 ]; then echo "ERROR: too many arguments: $#"; exit; fi
 
 for num_nodes in 2 4 6 8 10 12 14 16
 do
-# multicast
-for i in `seq 20 30`;
-do
-	for test_index in `seq 1 $1`;
-	do
-		obj_size=$((2**$i))
-		./multicast_test.sh $num_nodes $obj_size
-		pushd mpi
-		./mpi_broadcast.sh $num_nodes $obj_size > ../mpi_log/multicast-$num_nodes-$obj_size-$test_index
-		popd
+
+for test_name in "multicast reduce allreduce gather allgather"; do
+	for i in `seq 20 30`; do
+		for test_index in `seq 1 $1`; do
+			obj_size=$((2**$i))
+			./run_test.sh $(test_name)_test $num_nodes $obj_size
+			pushd mpi
+			./mpi_$test_name.sh $num_nodes $obj_size > ../mpi_log/$test_name-$num_nodes-$obj_size-$test_index
+			popd
+		done
 	done
 done
 
-# reduce
-for i in `seq 20 30`;
-do
-	for test_index in `seq 1 $1`;
-	do
-		obj_size=$((2**$i))
-		./reduce_test.sh $num_nodes $obj_size
-		pushd mpi
-		./mpi_reduce.sh $num_nodes $obj_size > ../mpi_log/reduce-$num_nodes-$obj_size-$test_index
-		popd
-	done
-done
-
-# allreduce
-for i in `seq 20 30`;
-do
-	for test_index in `seq 1 $1`;
-	do
-		obj_size=$((2**$i))
-		./allreduce_test.sh $num_nodes $obj_size
-		pushd mpi
-		./mpi_allreduce.sh $num_nodes $obj_size > ../mpi_log/allreduce-$num_nodes-$obj_size-$test_index
-		popd
-	done
-done
 done
