7890164f6953af0504a86c8751a099b7a4bb8d3d
Siyuan <suquark@gmail.com>
2020-01-15 21:17:09 -0800

    fix type converting bug

--- src/util/test_utils.h
+++ src/util/test_utils.h
@@ -75,7 +75,7 @@ float get_uniform_random_float(const std::string &seed_str) {
 std::shared_ptr<Buffer> get_random_float_buffer(size_t n_elements,
                                                 const std::string &seed_str) {
   auto buf = std::make_shared<Buffer>(n_elements * sizeof(float));
-  auto data = buf->MutableData();
+  float* data = (float*)buf->MutableData();
   float random_number = get_uniform_random_float(seed_str);
   for (int64_t i = 0; i < n_elements; i++) {
     data[i] = i * random_number;
