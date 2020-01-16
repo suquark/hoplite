c58c473c695983a7dd31d09450e685f94abdc64c
Siyuan <suquark@gmail.com>
2020-01-15 23:53:50 -0800

    lint

--- auto_test.sh
+++ auto_test.sh
@@ -7,14 +7,14 @@ do
 
 for test_name in multicast reduce allreduce gather allgather; do
   for i in `seq 20 30`; do
-		for test_index in `seq 1 $1`; do
-			obj_size=$((2**$i))
-			./run_test.sh $(test_name)_test $num_nodes $obj_size
-			pushd mpi
-			./mpi_$test_name.sh $num_nodes $obj_size > ../mpi_log/$test_name-$num_nodes-$obj_size-$test_index
-			popd
-		done
-	done
+    for test_index in `seq 1 $1`; do
+      obj_size=$((2**$i))
+      ./run_test.sh $(test_name)_test $num_nodes $obj_size
+      pushd mpi
+      ./mpi_$test_name.sh $num_nodes $obj_size > ../mpi_log/$test_name-$num_nodes-$obj_size-$test_index
+      popd
+    done
+  done
 done
 
 done
