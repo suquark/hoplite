41574414dcd7a216381fd0972a5d510ecae0ad36
Danyang Zhuo <danyangzhuo@gmail.com>
2020-01-16 09:57:32 -0800

    ray reduce (#49)

--- python/test_functions.py
+++ python/test_functions.py
@@ -12,7 +12,7 @@ import utils
 @ray.remote(resources={'node': 1}, max_calls=1)
 def ray_multicast(args_dict, world_rank, object_size):
     store = utils.create_store_using_dict(args_dict)
-    object_id = ray.ObjectID(b'\1' * 20);
+    object_id = ray.ObjectID(b'\0' * 20);
     if world_rank == 0:
         ray.worker.global_worker.core_worker.free_objects([object_id], False, True);
         array = np.random.randint(2**30, size=object_size//4, dtype=np.int32)
@@ -31,6 +31,41 @@ def ray_multicast(args_dict, world_rank, object_size):
         print(array)
     time.sleep(30)
 
+@ray.remote(resources={'node': 1}, max_calls=1)
+def ray_reduce(args_dict, world_size, world_rank, object_size):
+    store = utils.create_store_using_dict(args_dict)
+    object_id = ray.ObjectID(str(world_rank).encode().rjust(20, b'\0'));
+    ray.worker.global_worker.core_worker.free_objects([object_id], False, True);
+    time.sleep(5)
+    array = np.random.randint(2**30, size=object_size//4, dtype=np.int32)
+    ray.worker.global_worker.put_object(object_id, array)
+    buffer = store_lib.Buffer.from_buffer(array)
+    print("Buffer created, hash =", hash(buffer))
+    time.sleep(5)
+    if world_rank == 0:
+        object_ids = []
+        for i in range(0, world_size):
+            object_ids.append(ray.ObjectID(str(i).encode().rjust(20, b'\0')))
+        start = time.time()
+        ready_set, unready_set = ray.wait(object_ids, num_returns=1, timeout=5)
+        first_object = True
+        while True:
+            assert ready_set
+            array = ray.get(ready_set[0])
+            if first_object:
+                reduce_result = array
+                first_object = False
+            else:
+                reduce_result = reduce_result + array
+            if not unready_set:
+                break
+            ready_set, unready_set = ray.wait(unready_set, num_returns=1, timeout=5)
+        duration = time.time() - start
+        buffer = store_lib.Buffer.from_buffer(reduce_result)
+        print("Reduce completed, hash =", hash(buffer), "duration =", duration)
+        print(reduce_result)
+
+    time.sleep(30)
 
 @ray.remote(resources={'node': 1}, max_calls=1)
 def multicast(args_dict, world_rank, object_size):
