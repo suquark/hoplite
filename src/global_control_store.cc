3f3c80465c68fdd60c73093106511be3c66c4d3c
Zhuohan Li <zhuohan123@vip.qq.com>
2020-01-16 17:35:26 -0800

    [WIP] refactor global control store

--- src/global_control_store.cc
+++ src/global_control_store.cc
@@ -8,169 +8,127 @@
 #include "global_control_store.h"
 #include "logging.h"
 
-using objectstore::GetObjectLocationReply;
-using objectstore::GetObjectLocationRequest;
-using objectstore::ObjectCompleteReply;
-using objectstore::ObjectCompleteRequest;
-using objectstore::ObjectIsReadyReply;
-using objectstore::ObjectIsReadyRequest;
-using objectstore::SubscriptionReply;
-using objectstore::SubscriptionRequest;
-using objectstore::UnsubscriptionReply;
-using objectstore::UnsubscriptionRequest;
-using objectstore::WriteObjectLocationReply;
-using objectstore::WriteObjectLocationRequest;
+using objectstore::WriteLocationReply;
+using objectstore::WriteLocationRequest;
+using objectstore::GetLocationSyncReply;
+using objectstore::GetLocationSyncRequest;
+using objectstore::GetLocationAsyncReply;
+using objectstore::GetLocationAsyncRequest;
+using objectstore::GetLocationAsyncAnswerRequest;
+using objectstore::GetLocationAsyncAnswerReply;
 
 class NotificationListenerImpl final
     : public objectstore::NotificationListener::Service {
 public:
   NotificationListenerImpl(
-      std::unordered_set<ObjectNotifications *> &object_notifications)
+      std::unordered_map<std::string, std::shared_ptr<ObjectNotifications>> &object_notifications_pool)
       : objectstore::NotificationListener::Service(),
-        object_notifications_(object_notifications) {}
+        object_notifications_(object_notifications_pool), 
+        notifications_pool_mutex_(notifications_pool_mutex){}
 
-  grpc::Status ObjectIsReady(grpc::ServerContext *context,
-                             const ObjectIsReadyRequest *request,
-                             ObjectIsReadyReply *reply) {
-    for (auto notifications : object_notifications_) {
-      notifications->ReceiveObjectNotification(
-          ObjectID::FromBinary(request->object_id()));
+  grpc::Status GetLocationAsyncAnswer(grpc::ServerContext *context,
+                                      const GetLocationAsyncAnswerRequest *request,
+                                      GetLocationAsyncAnswerReply *reply) {
+    ObjectID object_id = ObjectID::FromBinary(request->object_id());
+    std::string sender_ip = request->sender_ip();
+    std::string query_id = request->query_id();
+    std::shared_ptr<ObjectNotifications> notifications;
+    {
+      std::lock_guard<std::mutex> guard(*notifications_pool_mutex_);
+      notifications = object_notifications_pool_[query_id];
     }
+    notifications->ReceiveObjectNotification(object_id, sender_ip);
     reply->set_ok(true);
     return grpc::Status::OK;
   }
 
 private:
-  std::unordered_set<ObjectNotifications *> &object_notifications_;
+  std::unordered_map<std::string, std::shared_ptr<ObjectNotifications>> &object_notifications_pool_;
+  std::shared_ptr<std::mutex> notifications_pool_mutex_;
 };
 
-ObjectNotifications::ObjectNotifications(std::vector<ObjectID> object_ids) {
-  for (auto object_id : object_ids) {
-    pending_.insert(object_id);
-  }
-}
-
-std::vector<ObjectID> ObjectNotifications::GetNotifications() {
+std::vector<std::pair<ObjectID, std::string>> ObjectNotifications::GetNotifications() {
   std::unique_lock<std::mutex> l(notification_mutex_);
-
-  std::vector<ObjectID> notifications;
   notification_cv_.wait(l, [this]() { return !ready_.empty(); });
-  for (auto &object_id : ready_) {
-    notifications.push_back(object_id);
-  }
+  std::vector<std::pair<ObjectID, std::string>> notifications = ready_;
   ready_.clear();
   return notifications;
 }
 
-void ObjectNotifications::ReceiveObjectNotification(const ObjectID &object_id) {
+void ObjectNotifications::ReceiveObjectNotification(const ObjectID &object_id, const std::string &sender_ip) {
   std::unique_lock<std::mutex> l(notification_mutex_);
-  if (pending_.find(object_id) == pending_.end()) {
-    return;
-  }
-  pending_.erase(object_id);
-  ready_.insert(object_id);
+  ready_.insert(std::make_pair(object_id, sender_ip));
   l.unlock();
   notification_cv_.notify_one();
 }
 
 GlobalControlStoreClient::GlobalControlStoreClient(
-    const std::string &redis_address, int port, const std::string &my_address,
+    const std::string &notification_server_address, const std::string &my_address, 
     int notification_port, int notification_listen_port)
-    : redis_address_(redis_address), my_address_(my_address),
-      notification_port_(notification_port),
+    : notification_server_address_(notification_server_address_), my_address_(my_address),
+      notification_server_port_(notification_server_port),
       notification_listen_port_(notification_listen_port),
       service_(std::make_shared<NotificationListenerImpl>(notifications_)) {
   std::string grpc_address =
-      my_address + ":" + std::to_string(notification_listen_port);
+      my_address + ":" + std::to_string(notification_listen_port_);
   grpc::ServerBuilder builder;
   builder.AddListeningPort(grpc_address, grpc::InsecureServerCredentials());
   builder.RegisterService(&*service_);
   grpc_server_ = builder.BuildAndStart();
   auto remote_notification_server_address =
-      redis_address_ + ":" + std::to_string(notification_port_);
+      notification_server_address_ + ":" + std::to_string(notification_port_);
   notification_channel_ = grpc::CreateChannel(
       remote_notification_server_address, grpc::InsecureChannelCredentials());
   notification_stub_ =
       objectstore::NotificationServer::NewStub(notification_channel_);
 }
 
-void GlobalControlStoreClient::write_object_location(
-    const ObjectID &object_id, const std::string &my_address) {
-  TIMELINE("write_object_location");
-  LOG(INFO) << "[RedisClient] Adding object " << object_id.Hex()
-            << " to Redis with address = " << my_address << ".";
+void GlobalControlStoreClient::WriteLocation(
+    const ObjectID &object_id, const std::string &sender_ip, bool finished) {
+  TIMELINE("GlobalControlStoreClient::WriteLocation");
+  LOG(INFO) << "[GlobalControlStoreClient] Adding object " << object_id.Hex()
+            << " to notification server with address = " << my_address << ".";
   grpc::ClientContext context;
-  WriteObjectLocationRequest request;
-  WriteObjectLocationReply reply;
+  WriteLocationRequest request;
+  WriteLocationReply reply;
   request.set_object_id(object_id.Binary());
-  request.set_ip(my_address);
+  request.set_sender_ip(my_address);
+  request.set_finished(finished);
   notification_stub_->WriteObjectLocation(&context, request, &reply);
-  DCHECK(reply.ok()) << "WriteWriteObjectLocation for " << object_id.ToString()
+  DCHECK(reply.ok()) << "WriteLocation for " << object_id.ToString()
                      << " failed.";
 }
 
 std::string
-GlobalControlStoreClient::get_object_location(const ObjectID &object_id) {
-  TIMELINE("get_object_location");
+GlobalControlStoreClient::GetLocationSync(const ObjectID &object_id) {
+  TIMELINE("GetLocationSync");
   grpc::ClientContext context;
-  GetObjectLocationRequest request;
-  GetObjectLocationReply reply;
+  GetLocationSyncRequest request;
+  GetLocationSyncReply reply;
   request.set_object_id(object_id.Binary());
-  notification_stub_->GetObjectLocation(&context, request, &reply);
-  return std::string(reply.ip());
+  notification_stub_->GetLocationSync(&context, request, &reply);
+  return std::string(reply.sender_ip());
 }
 
-ObjectNotifications *GlobalControlStoreClient::subscribe_object_locations(
-    const std::vector<ObjectID> &object_ids, bool include_completed_objects) {
-  ObjectNotifications *notifications = new ObjectNotifications(object_ids);
+std::shared_ptr<ObjectNotifications>
+GlobalControlStoreClient::GetLocationAsync(
+    const std::vector<ObjectID> &object_ids, const string& query_id) {
+  std::shared_ptr<ObjectNotifications> notifications = std::make_shared<ObjectNotifications>();
   {
-    std::lock_guard<std::mutex> guard(gcs_mutex_);
-    notifications_.insert(notifications);
+    std::lock_guard<std::mutex> guard(*notifications_pool_mutex_);
+    notifications_pool_[query_id] = notifications;
   }
-
-  for (auto object_id : object_ids) {
-    grpc::ClientContext context;
-    SubscriptionRequest request;
-    SubscriptionReply reply;
-    request.set_subscriber_ip(my_address_);
-    request.set_object_id(object_id.Binary());
-    notification_stub_->Subscribe(&context, request, &reply);
-
-    DCHECK(reply.ok()) << "Subscribing object " << object_id.Hex()
-                       << " failed.";
-  }
-
-  if (include_completed_objects) {
-    for (auto object_id : object_ids) {
-      if ("" != get_object_location(object_id)) {
-        notifications->ReceiveObjectNotification(object_id);
-      }
-    }
-  }
-
-  return notifications;
-}
-
-void GlobalControlStoreClient::unsubscribe_object_locations(
-    ObjectNotifications *notifications) {
-  std::lock_guard<std::mutex> guard(gcs_mutex_);
-
-  notifications_.erase(notifications);
-
-  delete notifications;
-}
-
-void GlobalControlStoreClient::PublishObjectCompletionEvent(
-    const ObjectID &object_id) {
-
   grpc::ClientContext context;
-  ObjectCompleteRequest request;
-  ObjectCompleteReply reply;
-  request.set_object_id(object_id.Binary());
-  auto status = notification_stub_->ObjectComplete(&context, request, &reply);
-  DCHECK(status.ok()) << "ObjectComplete gRPC failure, message: "
-                      << status.error_message();
-  DCHECK(reply.ok()) << "Object completes " << object_id.Hex() << " failed.";
+  GetLocationAsyncRequest request;
+  GetLocationAsyncReply reply;
+  request.set_receiver_ip(my_address_);
+  request.set_query_id(query_id);
+  for (auto object_id : object_ids) {
+    request.add_object_ids(object_id.Binary());
+  }
+  notification_stub_->GetLocationAsync(&context, request, &reply);
+  DCHECK(reply.ok()) << "GetLocationAsync failed.";
+  return notifications;
 }
 
 void GlobalControlStoreClient::worker_loop() {
--- src/global_control_store.h
+++ src/global_control_store.h
@@ -10,6 +10,7 @@
 #include <thread>
 #include <unordered_set>
 #include <vector>
+#include <utility>
 
 struct redisContext;
 
@@ -17,38 +18,31 @@ struct NotificationListenerImpl;
 
 class ObjectNotifications {
 public:
-  ObjectNotifications(std::vector<ObjectID> object_ids);
   std::vector<ObjectID> GetNotifications();
 
-  void ReceiveObjectNotification(const ObjectID &object_id);
+  void ReceiveObjectNotification(const ObjectID &object_id, const std::string &sender_ip);
 
 private:
   std::mutex notification_mutex_;
   std::condition_variable notification_cv_;
-  std::unordered_set<ObjectID> pending_;
-  std::unordered_set<ObjectID> ready_;
+  std::vector<std::pair<ObjectID, std::string>> ready_;
 };
 
 class GlobalControlStoreClient {
 public:
-  GlobalControlStoreClient(const std::string &redis_address, int redis_port,
+  GlobalControlStoreClient(const std::string &notification_server_address,
                            const std::string &my_address, int notification_port,
                            int notification_listen_port);
 
-  // Write object location to Redis server.
-  void write_object_location(const ObjectID &object_id,
-                             const std::string &my_address);
+  // Write object location to the notification server.
+  void WriteLocation(const ObjectID &object_id,
+                     const std::string &my_address);
 
-  // Get object location from Redis server.
-  std::string get_object_location(const ObjectID &object_id);
+  // Get object location from the notification server.
+  std::string GetLocationSync(const ObjectID &object_id);
 
-  ObjectNotifications *
-  subscribe_object_locations(const std::vector<ObjectID> &object_ids,
-                             bool include_completed_objects = false);
-
-  void unsubscribe_object_locations(ObjectNotifications *notifications);
-
-  void PublishObjectCompletionEvent(const ObjectID &object_id);
+  std::shared_ptr<ObjectNotifications>
+  GetLocationAsync(const std::vector<ObjectID> &object_ids);
 
   inline std::thread Run() {
     std::thread notification_thread(&GlobalControlStoreClient::worker_loop,
@@ -59,15 +53,15 @@ public:
 private:
   void worker_loop();
 
-  std::mutex gcs_mutex_;
-  const std::string &redis_address_;
+  const std::string &notification_server_address_;
   const std::string &my_address_;
-  const int notification_port_;
+  const int notification_server_port_;
   const int notification_listen_port_;
   std::shared_ptr<grpc::Channel> notification_channel_;
   std::unique_ptr<objectstore::NotificationServer::Stub> notification_stub_;
 
-  std::unordered_set<ObjectNotifications *> notifications_;
+  std::mutex notifications_pool_mutex_;
+  std::unordered_map<std::string, std::shared_ptr<ObjectNotifications>> notifications_pool_;
 
   std::unique_ptr<grpc::Server> grpc_server_;
   std::shared_ptr<NotificationListenerImpl> service_;
