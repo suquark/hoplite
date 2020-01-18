16cab446277bcb65cba15f9aa2f8277e8f031661
Zhuohan Li <zhuohan123@vip.qq.com>
2020-01-18 15:22:29 -0800

    debug

--- src/object_control.h
+++ src/object_control.h
@@ -14,8 +14,11 @@ class ObjectStoreServiceImpl;
 
 class GrpcServer {
 public:
-  GrpcServer(ObjectSender &object_sender, LocalStoreClient &local_store_client,
-             ObjectStoreState &state, const std::string &ip, int port);
+  GrpcServer(ObjectSender &object_sender,
+                       LocalStoreClient &local_store_client, 
+                       GlobalControlStoreClient &gcs_client,
+                       ObjectStoreState &state, const std::string &my_address,
+                       int port);
 
   inline std::thread Run() {
     return std::thread(&GrpcServer::worker_loop, this);
