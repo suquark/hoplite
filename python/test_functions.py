49d95e54ee7912316804fe20fa064bb1b2259bc4
Danyang Zhuo <danyangzhuo@gmail.com>
2020-01-18 16:59:42 -0800

    Add ray allreduce, gather, and allgather (#51)
    * ray allreduce
    
    * Ray gather & allgather

--- python/test_functions.py
+++ python/test_functions.py
@@ -68,6 +68,116 @@ def ray_reduce(args_dict, world_size, world_rank, object_size):
     time.sleep(30)
 
 @ray.remote(resources={'node': 1}, max_calls=1)
+def ray_allreduce(args_dict, world_size, world_rank, object_size):
+    store = utils.create_store_using_dict(args_dict)
+    object_id = ray.ObjectID(str(world_rank).encode().rjust(20, b'\0'))
+    reduce_id = ray.ObjectID(str(world_size).encode().rjust(20, b'\0'))
+    if world_rank == 0:
+        ray.worker.global_worker.core_worker.free_objects([reduce_id], False, True)
+    ray.worker.global_worker.core_worker.free_objects([object_id], False, True)
+    time.sleep(5)
+    array = np.random.randint(2**30, size=object_size//4, dtype=np.int32)
+    ray.worker.global_worker.put_object(object_id, array)
+    buffer = store_lib.Buffer.from_buffer(array)
+    print("Buffer created, hash =", hash(buffer))
+    time.sleep(5)
+    start = time.time()
+    if world_rank == 0:
+        object_ids = []
+        for i in range(0, world_size):
+            object_ids.append(ray.ObjectID(str(i).encode().rjust(20, b'\0')))
+        ready_set, unready_set = ray.wait(object_ids, num_returns=1, timeout=5)
+        first_object = True
+        while True:
+            assert ready_set
+            array = ray.get(ready_set[0])
+            if first_object:
+                reduce_result = array
+                first_object = False
+            else:
+                reduce_result = reduce_result + array
+            if not unready_set:
+                break
+            ready_set, unready_set = ray.wait(unready_set, num_returns=1, timeout=5)
+        ray.worker.global_worker.put_object(reduce_id, reduce_result)
+
+    ready_set, unready_set = ray.wait([reduce_id], num_returns=1, timeout=20)
+    allreduce_result = ray.get(ready_set[0])
+    duration = time.time() - start
+    buffer = store_lib.Buffer.from_buffer(allreduce_result)
+    print("Allreduce completed, hash =", hash(buffer), "duration =", duration)
+    print(allreduce_result)
+
+    time.sleep(30)
+
+@ray.remote(resources={'node': 1}, max_calls=1)
+def ray_gather(args_dict, world_size, world_rank, object_size):
+    store = utils.create_store_using_dict(args_dict)
+    object_id = ray.ObjectID(str(world_rank).encode().rjust(20, b'\0'))
+    ray.worker.global_worker.core_worker.free_objects([object_id], False, True)
+    time.sleep(5)
+    array = np.random.randint(2**30, size=object_size//4, dtype=np.int32)
+    ray.worker.global_worker.put_object(object_id, array)
+    buffer = store_lib.Buffer.from_buffer(array)
+    print("Buffer created, hash =", hash(buffer))
+    time.sleep(5)
+    if world_rank == 0:
+        object_ids = []
+        for i in range(0, world_size):
+            object_ids.append(ray.ObjectID(str(i).encode().rjust(20, b'\0')))
+        start = time.time()
+        ready_set, unready_set = ray.wait(object_ids, num_returns=1, timeout=5)
+        gather_result = []
+        while True:
+            assert ready_set
+            array = ray.get(ready_set[0])
+            gather_result.append(array)
+            if not unready_set:
+                break
+            ready_set, unready_set = ray.wait(unready_set, num_returns=1, timeout=5)
+        duration = time.time() - start
+        hash_sum = 0
+        for array in gather_result:
+            buffer = store_lib.Buffer.from_buffer(array)
+            hash_sum += hash(buffer)
+        print("Gather completed, hash =", hash_sum, "duration =", duration)
+
+    time.sleep(30)
+
+@ray.remote(resources={'node': 1}, max_calls=1)
+def ray_allgather(args_dict, world_size, world_rank, object_size):
+    store = utils.create_store_using_dict(args_dict)
+    object_id = ray.ObjectID(str(world_rank).encode().rjust(20, b'\0'))
+    ray.worker.global_worker.core_worker.free_objects([object_id], False, True)
+    time.sleep(5)
+    array = np.random.randint(2**30, size=object_size//4, dtype=np.int32)
+    ray.worker.global_worker.put_object(object_id, array)
+    buffer = store_lib.Buffer.from_buffer(array)
+    print("Buffer created, hash =", hash(buffer))
+    time.sleep(5)
+    object_ids = []
+    for i in range(0, world_size):
+        object_ids.append(ray.ObjectID(str(i).encode().rjust(20, b'\0')))
+    start = time.time()
+    ready_set, unready_set = ray.wait(object_ids, num_returns=1, timeout=5)
+    gather_result = []
+    while True:
+        assert ready_set
+        array = ray.get(ready_set[0])
+        gather_result.append(array)
+        if not unready_set:
+            break
+        ready_set, unready_set = ray.wait(unready_set, num_returns=1, timeout=5)
+    duration = time.time() - start
+    hash_sum = 0
+    for array in gather_result:
+        buffer = store_lib.Buffer.from_buffer(array)
+        hash_sum += hash(buffer)
+    print("Allgather completed, hash =", hash_sum, "duration =", duration)
+
+    time.sleep(30)
+
+@ray.remote(resources={'node': 1}, max_calls=1)
 def multicast(args_dict, world_rank, object_size):
     store = utils.create_store_using_dict(args_dict)
     object_id = store_lib.ObjectID(b'\0' * 20)
