0cb5b14011d844739e75e3d54760f072f3df411e
Siyuan <suquark@gmail.com>
2020-01-20 23:22:48 -0800

    fix

--- init_env.py
+++ init_env.py
@@ -4,12 +4,12 @@ import os
 import ray
 import sys
 
-current_directory = os.path.abspath(os.path.curdir)
-print(current_directory)
+root_directory = os.path.dirname(os.path.abspath(__file__))
+print(root_directory)
 
 dirs = [
-    os.path.join(current_directory, 'python'),
-    os.path.join(current_directory, 'app', 'parameter-server'),
+    os.path.join(root_directory, 'python'),
+    os.path.join(root_directory, 'app', 'parameter-server'),
 ]
 
 ray.init(address='auto', load_code_from_local=True)
