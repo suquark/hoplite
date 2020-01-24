46c9af1c04ca5b3633a6bb3f17dcb8dacfcb389e
Danyang Zhuo <danyangzhuo@gmail.com>
2020-01-24 01:56:42 +0000

    auto test & parse ray result

---
+++ python/auto_test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [ "$#" -lt 1 ]; then echo "ERROR: number of tests"; exit; fi
+if [ "$#" -gt 2 ]; then echo "ERROR: too many arguments: $#"; exit; fi
+
+mkdir -p log
+
+for num_nodes in 8
+do
+
+for test_name in multicast reduce allreduce; do
+  for i in `seq 20 30`; do
+    for test_index in `seq 1 $1`; do
+      obj_size=$((2**$i))
+      filename=log/$test_name-$num_nodes-$obj_size.log
+      if [ ! -f $filename ]; then
+         ./launch_test.py -t ray-${test_name} -n $num_nodes -s $obj_size 2>&1 | tee $filename
+      fi
+    done
+  done
+done
+
+done
---
+++ python/parse_ray_result.py
@@ -0,0 +1,118 @@
+import os
+import sys
+
+def parse_multicast(folder_path):
+    last_retrieval_time = 0
+    try:
+        f = open(folder_path, 'r')
+        for line in f.readlines():
+            if 'during =' in line:
+                tmp = line.split('during =')[1]
+                retrieval_time = float(tmp)
+                if retrieval_time > last_retrieval_time:
+                    last_retrieval_time = retrieval_time
+
+        f.close()
+    except:
+        print (folder_path)
+    try:
+        a = retrieval_time
+    except:
+        print (folder_path)
+    return last_retrieval_time
+
+def parse_reduce(folder_path):
+    try:
+        f = open(folder_path, 'r')
+        for line in f.readlines():
+            if 'duration =' in line:
+                tmp = line.split('duration =')[1]
+                retrieval_time = float(tmp)
+        f.close()
+    except:
+        print (folder_path)
+    try:
+        a = retrieval_time
+    except:
+        print (folder_path)
+    return retrieval_time
+
+def parse_allreduce(folder_path):
+    last_retrieval_time = 0
+    try:
+        f = open(folder_path, 'r')
+        for line in f.readlines():
+            if 'duration =' in line:
+                tmp = line.split('duration =')[1]
+                retrieval_time = float(tmp)
+                if retrieval_time > last_retrieval_time:
+                    last_retrieval_time = retrieval_time
+
+        f.close()
+    except:
+        print (folder_path)
+    try:
+        a = retrieval_time
+    except:
+        print (folder_path)
+    return last_retrieval_time
+
+
+def parse_file(task_name, log_dir, foldername):
+    path = os.path.join(log_dir, foldername)
+
+    if task_name == 'multicast':
+        return parse_multicast(path)
+
+    if task_name == 'reduce':
+        return parse_reduce(path)
+
+    if task_name == 'allreduce':
+        return parse_allreduce(path)
+
+    assert (False)
+
+def main(log_dir):
+    files = os.listdir(log_dir)
+
+    tasks = {}
+
+    for filename in files:
+        splited = filename.split('-');
+        if len(splited) != 3:
+            exit(-1)
+        task_name = splited[0]
+        number_of_nodes = splited[1]
+        object_size = splited[2].split('.')[0]
+        
+        task = task_name + '-' + number_of_nodes + '-' + object_size
+
+        if task not in tasks:
+            tasks[task] = []
+
+        tasks[task].append(filename)
+
+    results = {}
+    
+    for task in tasks:
+        result = 0
+        for foldername in tasks[task]:
+            result += parse_file(task.split('-')[0], log_dir, foldername);
+        result /= len(tasks[task])
+
+        results[task] = result
+
+    task_list = []
+    for task in results:
+        task_list.append(task)
+
+    task_list = sorted(task_list, reverse=True)
+
+    for task in task_list:
+        print (task.replace('-',','), ',', results[task])
+
+
+if __name__ == "__main__":
+    assert len(sys.argv) == 2, "Usage: python parse_result.py LOG_DIR"
+    log_dir = sys.argv[1]
+    main(log_dir)
