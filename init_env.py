2eea26e10dc5f19e83331378aa11697d126e6525
Danyang Zhuo <danyangzhuo@gmail.com>
2020-02-03 22:16:02 -0800

    Update init_env.py
    Co-Authored-By: Zhuohan Li <zhuohan123@vip.qq.com>

--- init_env.py
+++ init_env.py
@@ -15,4 +15,4 @@ dirs = [
 ray.init(address='auto', load_code_from_local=True)
 
 ray.worker.global_worker.run_function_on_all_workers(
-    lambda worker_info____: [sys.path.insert(1, d) for d in dirs])
+    lambda worker_info: [sys.path.insert(1, d) for d in dirs])
