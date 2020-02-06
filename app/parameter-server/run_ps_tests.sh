79681852d0275b3c10b395a2fcafcd8571f4f1c0
Zhuohan Li <zhuohan123@vip.qq.com>
2020-02-06 14:42:07 -0800

    finish script

--- app/parameter-server/run_ps_tests.sh
+++ app/parameter-server/run_ps_tests.sh
@@ -1,14 +1,17 @@
 mkdir -p ps-log/
 
 for n_nodes in 8 16; do
-    # pkill notification
-    # /home/ubuntu/efs/zhuohan/object_store/restart_all_workers.sh 
-    # python parameter_server.py -n $(($n_nodes - 1)) --no-test | tee ps-log/sync-$n_nodes-hoplite.log
+    echo "==========" sync-$n_nodes-hoplite "=========="
+    pkill notification
+    /home/ubuntu/efs/zhuohan/object_store/restart_all_workers.sh 
+    python parameter_server.py -n $(($n_nodes - 1)) --no-test | tee ps-log/sync-$n_nodes-hoplite.log
 
-    # pkill notification
-    # /home/ubuntu/efs/zhuohan/object_store/restart_all_workers.sh 
-    # python ray_parameter_server_baseline.py -n $(($n_nodes - 1)) --no-test | tee ps-log/sync-$n_nodes-ray.log
+    echo "==========" sync-$n_nodes-ray "=========="
+    pkill notification
+    /home/ubuntu/efs/zhuohan/object_store/restart_all_workers.sh 
+    python ray_parameter_server_baseline.py -n $(($n_nodes - 1)) --no-test | tee ps-log/sync-$n_nodes-ray.log
 
+    echo "==========" sync-$n_nodes-mpi "=========="
     worker_pubips=$(ray get-worker-ips ~/ray_bootstrap_config.yaml)
     master=$(ifconfig | grep 'inet.*broadcast' | awk '{print $2}')
     slaves=()
@@ -20,16 +23,18 @@ for n_nodes in 8 16; do
     echo Nodes: ${all_nodes[@]} "("${#all_nodes[@]}")"
 
     all_hosts=$(echo ${all_nodes[@]} | sed 's/ /,/g')
-    echo $all_hosts
-    # pkill notification
-    # /home/ubuntu/efs/zhuohan/object_store/restart_all_workers.sh 
-    # mpirun --map-by ppr:1:node -hosts $all_hosts python mpi_parameter_server.py --no-test | tee ps-log/sync-$n_nodes-mpi.log
+    
+    pkill notification
+    /home/ubuntu/efs/zhuohan/object_store/restart_all_workers.sh 
+    mpirun --map-by ppr:1:node -hosts $all_hosts python mpi_parameter_server.py --no-test | tee ps-log/sync-$n_nodes-mpi.log
+    
+    echo "==========" async-$n_nodes-hoplite "=========="
+    pkill notification
+    /home/ubuntu/efs/zhuohan/object_store/restart_all_workers.sh 
+    python parameter_server.py -n $(($n_nodes - 1)) -a $((($n_nodes - 1) / 2)) --no-test | tee ps-log/async-$n_nodes-hoplite.log
 
-    # pkill notification
-    # /home/ubuntu/efs/zhuohan/object_store/restart_all_workers.sh 
-    # python parameter_server.py -n $(($n_nodes - 1)) -a $((($n_nodes - 1) / 2)) --no-test | tee ps-log/async-$n_nodes-hoplite.log
-
-    # pkill notification
-    # /home/ubuntu/efs/zhuohan/object_store/restart_all_workers.sh 
-    # python ray_parameter_server_baseline.py -n $(($n_nodes - 1)) -a $((($n_nodes - 1) / 2)) --no-test | tee ps-log/async-$n_nodes-ray.log
+    echo "==========" async-$n_nodes-ray "=========="
+    pkill notification
+    /home/ubuntu/efs/zhuohan/object_store/restart_all_workers.sh 
+    python ray_parameter_server_baseline.py -n $(($n_nodes - 1)) -a $((($n_nodes - 1) / 2)) --no-test | tee ps-log/async-$n_nodes-ray.log
 done
