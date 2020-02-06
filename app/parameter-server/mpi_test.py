9a57d666f7d76b17dbec868d245dc490253f5b9e
Zhuohan Li <zhuohan123@vip.qq.com>
2020-02-06 11:34:41 -0800

    debug

--- app/parameter-server/mpi_test.py
+++ app/parameter-server/mpi_test.py
@@ -5,9 +5,9 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
 if rank == 0:
-    data = np.ones(10)
+    data = np.ones(10, dtype=np.float32)
 else:
-    data = None
+    data = np.empty(10, dtype=np.float32)
 
 comm.Bcast(data, root=0)
 print(rank, data)
