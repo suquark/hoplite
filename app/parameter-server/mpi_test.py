e1cac13db118bf3bee498e2439d9e629a2a82010
Zhuohan Li <zhuohan123@vip.qq.com>
2020-02-06 11:37:00 -0800

    debug

--- app/parameter-server/mpi_test.py
+++ app/parameter-server/mpi_test.py
@@ -6,8 +6,10 @@ rank = comm.Get_rank()
 
 if rank == 0:
     data = np.ones(10, dtype=np.float32)
+    data_sum = np.empty(10, dtype=np.float32)
 else:
-    data = np.empty(10, dtype=np.float32)
+    data = np.ones(10, dtype=np.float32)
+    data_sum = np.empty(10, dtype=np.float32)
 
-comm.Bcast(data, root=0)
-print(rank, data)
+comm.Reduce(data, data_sum, op=MPI.SUM root=0)
+print(rank, data, data_sum)
