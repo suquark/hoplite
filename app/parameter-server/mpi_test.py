ebb7f4ec8698a6f47b015a922d1fb00b7d826998
Zhuohan Li <zhuohan123@vip.qq.com>
2020-02-06 12:01:41 -0800

    debug mpi

--- app/parameter-server/mpi_test.py
+++ app/parameter-server/mpi_test.py
@@ -1,5 +1,6 @@
 import numpy as np
 from mpi4py import MPI
+import time
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
@@ -8,6 +9,7 @@ if rank == 0:
     data = np.ones(10, dtype=np.float32)
     data_sum = np.empty(10, dtype=np.float32)
 else:
+    time.sleep(rank * 3)
     data = np.ones(10, dtype=np.float32)
     data_sum = np.empty(10, dtype=np.float32)
 
