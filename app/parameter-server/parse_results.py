408ad7cef7730cbc1ddcee69c297f28e9fea7cc2
Zhuohan Li <zhuohan123@vip.qq.com>
2020-02-06 23:14:00 -0800

    test

--- app/parameter-server/parse_results.py
+++ app/parameter-server/parse_results.py
@@ -8,7 +8,7 @@ for log_file in sorted(os.listdir(log_dir)):
             if "step time:" in line:
                 all_time.append(float(line.split()[-1]))
         all_time = all_time[1:]
-        all_time = np.array(all_time[3:-5])
+        all_time = np.array(all_time[3:-3])
         if log_file.split('-')[0] == 'async':
             all_time = 8 * ((int(log_file.split('-')[1]) - 1) // 2) / all_time
         else:
@@ -16,4 +16,4 @@ for log_file in sorted(os.listdir(log_dir)):
         new_all_time = []
         for i in range(0, len(all_time), 4):
             new_all_time.append(np.mean(all_time[i:i + 4]))
-        print(log_file.ljust(20), np.mean(new_all_time), np.std(new_all_time), sep='\t')
+        print(log_file.ljust(20), np.mean(new_all_time), np.std(new_all_time), len(new_all_time), sep='\t')
