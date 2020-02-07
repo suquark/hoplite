7a24917f1de23c5fe0f29381c794b2b6d9e27a4c
Zhuohan Li <zhuohan123@vip.qq.com>
2020-02-06 19:47:36 -0800

    add result parser

---
+++ app/parameter-server/parse_results.py
@@ -0,0 +1,10 @@
+import numpy as np
+import os
+
+for log_file in os.listdir():
+    with open(log_file, "r") as f:
+        all_time = []
+        for line in f:
+            if "step time:" in line:
+                all_time.append(float(line.split()[-1]))
+        print(log_file, all_times)
