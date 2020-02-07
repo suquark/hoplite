a54a6e134af7c50c402ef2175ca8ef6cdf6a286a
Zhuohan Li <zhuohan123@vip.qq.com>
2020-02-06 20:19:21 -0800

    test

--- app/parameter-server/parse_results.py
+++ app/parameter-server/parse_results.py
@@ -9,4 +9,8 @@ for log_file in sorted(os.listdir(log_dir)):
                 all_time.append(float(line.split()[-1]))
         all_time = all_time[1:]
         all_time = all_time[1:-10]
+        if log_file.split('-')[0] == 'async':
+            all_time = 8 * ((int(log_file.split('-')[1]) - 1) // 2) / all_time
+        else:
+            all_time = 8 * int(log_file.split('-')[1] / all_time
         print(log_file.ljust(20), np.mean(all_time), np.std(all_time), sep='\t')
