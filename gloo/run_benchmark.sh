a4207ee8f5beea4142fb978d5db1266881f5b8a5
Zhuohan Li <zhuohan123@vip.qq.com>
2020-08-17 15:54:27 -0700

    add gloo benchmark (#87)
    * add gloo benchmark
    
    * lint

---
+++ gloo/run_benchmark.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# This script can run commands on all nodes on the cluster: ./fornode <commands>
+
+root_dir=$HOME/efs/object_store/
+source $root_dir/load_cluster_env.sh
+
+PIDS=()
+
+redis-server --port 7777 --protected-mode no &
+
+REDIS_PID=$!
+
+i=0
+echo $MY_IPADDR
+for node in ${ALL_IPADDR[@]}; do
+  echo "=> $node $i"
+  ssh -o StrictHostKeyChecking=no $node PATH=$PATH:/home/ubuntu/anaconda3/bin:/home/ubuntu/anaconda3/condabin, \
+  /home/ubuntu/efs/gloo/build/gloo/benchmark/benchmark \
+  --size 4 \
+  --rank $i \
+  --redis-host $MY_IPADDR \
+  --redis-port 7777 \
+  --prefix initial-benchmark \
+  --transport tcp \
+  --elements -1 \
+  --iteration-time 1s \
+  allreduce_ring_chunked &
+  PIDS+=($!)
+  i=$(($i + 1))
+done
+wait ${PIDS[@]}
+kill $REDIS_PID
