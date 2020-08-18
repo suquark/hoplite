a7fefce545292af38466cb77f4f78ae8154410e3
Zhuohan Li <zhuohan123@vip.qq.com>
2020-08-18 16:27:01 -0700

    add autotest_gloo

---
+++ gloo/auto_test_gloo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ "$#" -lt 1 ]; then echo "ERROR: number of tests"; exit; fi
+if [ "$#" -gt 2 ]; then echo "ERROR: too many arguments: $#"; exit; fi
+
+for num_nodes in 4 8 12 16
+do
+
+for test_name in allreduce_ring allreduce_ring_chunked allreduce_halving_doubling allreduce_bcube broadcast_one_to_all; do
+  for i in 10 15 20 25 30; do
+    for test_index in `seq 1 $1`; do
+      obj_size=$((2**$i))
+      ./run_benchmark.sh $test_name $num_nodes $obj_size
+    done
+  done
+done
+
+done
