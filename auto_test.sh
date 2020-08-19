a51383bd64b24966c14406d5e74dec1a8c608655
Zhuohan Li <zhuohan123@vip.qq.com>
2020-08-18 17:45:55 -0700

    fix original auto test

--- auto_test.sh
+++ auto_test.sh
@@ -4,16 +4,16 @@ if [ "$#" -gt 2 ]; then echo "ERROR: too many arguments: $#"; exit; fi
 
 mkdir -p mpi_log
 
-for num_nodes in 2 4 6 8 10 12 14 16
+for num_nodes in 4 8 12 16
 do
 
 for test_name in multicast reduce allreduce gather allgather; do
-  for i in `seq 20 30`; do
+  for i in 10 15 20 25 30; do
     for test_index in `seq 1 $1`; do
       obj_size=$((2**$i))
       ./run_test.sh ${test_name}_test $num_nodes $obj_size
       pushd mpi
-      ./mpi_$test_name.sh $num_nodes $obj_size > ../mpi_log/$test_name-$num_nodes-$obj_size-$test_index
+      ./mpi_run_test.sh $test_name $num_nodes $obj_size > ../mpi_log/$test_name-$num_nodes-$obj_size-$test_index
       popd
     done
   done
