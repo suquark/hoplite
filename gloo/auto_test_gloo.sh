115893c4f158eee0cb898a2fa486c1927b6b3909
Zhuohan Li <zhuohan123@vip.qq.com>
2020-08-18 17:52:21 -0700

    Autotest gloo (#88)
    * add gloo benchmark
    
    * update run_benchmark_script
    
    * log to file
    
    * add autotest_gloo
    
    * add a sleep
    
    * add get_worker_ips.py
    
    * modify load_cluster_env
    
    * fix load_cluster_env
    
    * add parser
    
    * fix parse script
    
    * fix original auto test

--- gloo/auto_test_gloo.sh
+++ gloo/auto_test_gloo.sh
@@ -2,19 +2,15 @@
 if [ "$#" -lt 1 ]; then echo "ERROR: number of tests"; exit; fi
 if [ "$#" -gt 2 ]; then echo "ERROR: too many arguments: $#"; exit; fi
 
-mkdir -p mpi_log
-
 for num_nodes in 4 8 12 16
 do
 
-for test_name in multicast reduce allreduce gather allgather; do
+for test_name in allreduce_ring allreduce_ring_chunked allreduce_halving_doubling allreduce_bcube broadcast_one_to_all; do
   for i in 10 15 20 25 30; do
     for test_index in `seq 1 $1`; do
       obj_size=$((2**$i))
-      ./run_test.sh ${test_name}_test $num_nodes $obj_size
-      pushd mpi
-      ./mpi_run_test.sh $test_name $num_nodes $obj_size > ../mpi_log/$test_name-$num_nodes-$obj_size-$test_index
-      popd
+      ./run_benchmark.sh $test_name $num_nodes $obj_size
+      sleep 0.1
     done
   done
 done
--- gloo/run_benchmark.sh
+++ gloo/run_benchmark.sh
@@ -1,31 +1,54 @@
 #!/bin/bash
 
-# This script can run commands on all nodes on the cluster: ./fornode <commands>
+if [ "$#" -lt 3 ]; then
+  echo "$(tput setaf 1)[ERROR]$(tput sgr 0) test name, number of nodes & input size required"
+  echo "test name: allreduce_ring, allreduce_ring_chunked, allreduce_halving_doubling, "
+  echo "           allreduce_bcube, barrier_all_to_all, broadcast_one_to_all, pairwise_exchange"
+  exit -1
+fi
 
-root_dir=$HOME/efs/object_store/
-source $root_dir/load_cluster_env.sh
+if [ "$#" -gt 6 ]; then
+  echo "$(tput setaf 1)[ERROR]$(tput sgr 0) too many arguments: $#"
+  exit -1
+fi
 
+# trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM SIGHUP EXIT
+
+test_name=$1
+world_size=$2
+object_size=$3
+
+ROOT_DIR=$HOME/efs/object_store/
+source $ROOT_DIR/load_cluster_env.sh
+ALL_IPADDR=(${ALL_IPADDR[@]:0:$world_size})
 PIDS=()
 
-redis-server --port 7777 --protected-mode no &
+GLOO_DIR=$HOME/efs/gloo/
 
+log_dir=$ROOT_DIR/gloo_log/$(date +"%Y%m%d-%H%M%S")-$test_name-$world_size-$object_size
+mkdir -p $log_dir
+ln -sfn $log_dir/ $ROOT_DIR/gloo_log/latest
+
+redis-server --port 7777 --protected-mode no &> /dev/null &
 REDIS_PID=$!
+sleep 1
 
 i=0
 echo $MY_IPADDR
 for node in ${ALL_IPADDR[@]}; do
   echo "=> $node $i"
-  ssh -o StrictHostKeyChecking=no $node PATH=$PATH:/home/ubuntu/anaconda3/bin:/home/ubuntu/anaconda3/condabin, \
-  /home/ubuntu/efs/gloo/build/gloo/benchmark/benchmark \
-  --size 4 \
-  --rank $i \
-  --redis-host $MY_IPADDR \
-  --redis-port 7777 \
-  --prefix initial-benchmark \
-  --transport tcp \
-  --elements -1 \
-  --iteration-time 1s \
-  allreduce_ring_chunked &
+  ssh -o StrictHostKeyChecking=no $node \
+    $GLOO_DIR/build/gloo/benchmark/benchmark \
+    --size $world_size \
+    --rank $i \
+    --redis-host $MY_IPADDR \
+    --redis-port 7777 \
+    --prefix benchmark-$test_name-$world_size-$object_size \
+    --transport tcp \
+    --elements $(($object_size / 4)) \
+    --iteration-count 1 \
+    $test_name \
+    2>&1 | tee $log_dir/$i.log &
   PIDS+=($!)
   i=$(($i + 1))
 done
