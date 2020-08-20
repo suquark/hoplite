a669b68884dc2c5d84b6b3f8e1042c1baaaba033
Zhuohan Li <zhuohan123@vip.qq.com>
2020-08-20 04:19:24 +0000

    wrong number of arguments in run_dask.sh

--- dask/run_dask.sh
+++ dask/run_dask.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-if [ "$#" -lt 2 ]; then echo "$(tput setaf 1)[ERROR]$(tput sgr 0) number of nodes"; exit -1; fi
-if [ "$#" -gt 4 ]; then echo "$(tput setaf 1)[ERROR]$(tput sgr 0) too many arguments: $#"; exit -1; fi
+if [ "$#" -lt 1 ]; then echo "$(tput setaf 1)[ERROR]$(tput sgr 0) number of nodes"; exit -1; fi
+if [ "$#" -gt 3 ]; then echo "$(tput setaf 1)[ERROR]$(tput sgr 0) too many arguments: $#"; exit -1; fi
 
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM SIGHUP EXIT
 
 ROOT_DIR=$(dirname $(dirname $(realpath -s $0)))
 world_size=$1
 
-if [ "$#" -eq 2 ]; then
+if [ "$#" -eq 1 ]; then
     source $ROOT_DIR/load_cluster_env.sh
     OTHERS_IPADDR=(${OTHERS_IPADDR[@]:0:$(($world_size-1))})
     
