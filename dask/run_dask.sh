f2e6bb2d47f7015eeed3d51cbfb68e05211c20a7
Danyang Zhuo <danyangzhuo@gmail.com>
2020-08-20 03:23:51 +0000

    run_dask

---
+++ dask/run_dask.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+if [ "$#" -lt 2 ]; then echo "$(tput setaf 1)[ERROR]$(tput sgr 0) number of nodes"; exit -1; fi
+if [ "$#" -gt 4 ]; then echo "$(tput setaf 1)[ERROR]$(tput sgr 0) too many arguments: $#"; exit -1; fi
+
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM SIGHUP EXIT
+
+ROOT_DIR=$(dirname $(dirname $(realpath -s $0)))
+world_size=$1
+
+if [ "$#" -eq 2 ]; then
+    source $ROOT_DIR/load_cluster_env.sh
+    OTHERS_IPADDR=(${OTHERS_IPADDR[@]:0:$(($world_size-1))})
+    
+    dask-scheduler &
+    sleep 1
+
+    for index in ${!OTHERS_IPADDR[@]}
+    do
+        rank=$((index+1))
+        ssh -t -t ${OTHERS_IPADDR[$index]} "$(realpath -s $0) $MY_IPADDR $rank" &
+    done
+
+    dask-worker $MY_IPADDR:8786 --name Dask-0
+else
+    master=$1
+    index=$2
+    dask-worker $master:8786 --name Dask-$index
+fi
