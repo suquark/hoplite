72ff7139d292c8f355e5d67ebdbebc40f748462d
Zhuohan Li <zhuohan123@vip.qq.com>
2020-08-24 22:07:54 -0700

    add n_trails for all_reduce

--- allreduce_test.cc
+++ allreduce_test.cc
@@ -17,6 +17,7 @@ int main(int argc, char **argv) {
   int64_t world_size = std::strtoll(argv[3], NULL, 10);
   int64_t rank = std::strtoll(argv[4], NULL, 10);
   int64_t object_size = std::strtoll(argv[5], NULL, 10);
+  int64_t n_trials = std::strtoll(argv[6], NULL, 10);
 
   ::ray::RayLog::StartRayLog(my_address, ::ray::RayLogLevel::DEBUG);
 
@@ -28,33 +29,35 @@ int main(int argc, char **argv) {
 
   std::thread exit_thread(timed_exit, 30);
 
-  ObjectID reduction_id = object_id_from_suffix("ffffffff");
-  std::vector<ObjectID> object_ids;
-  float sum = 0;
-  for (int i = 0; i < world_size; i++) {
-    auto oid = object_id_from_integer(i);
-    object_ids.push_back(oid);
-    auto rnum = get_uniform_random_float(oid.Hex());
-    sum += rnum;
+  for (int trial = 0; trial < n_trials; trial++) {
+    ObjectID reduction_id = object_id_from_integer(trial * 1000000 + 99999);
+    std::vector<ObjectID> object_ids;
+    float sum = 0;
+    for (int i = 0; i < world_size; i++) {
+      auto oid = object_id_from_integer(trial * 1000000 + i);
+      object_ids.push_back(oid);
+      auto rnum = get_uniform_random_float(oid.Hex());
+      sum += rnum;
+    }
+    DCHECK(object_size % sizeof(float) == 0);
+
+    ObjectID rank_object_id = object_ids[rank];
+    std::shared_ptr<Buffer> reduction_result;
+
+    put_random_buffer<float>(store, rank_object_id, object_size);
+    barrier(rank, redis_address, 7777, world_size, my_address);
+
+    auto start = std::chrono::system_clock::now();
+    if (rank == 0) {
+      store.Reduce(object_ids, reduction_id);
+    }
+    store.Get(reduction_id, &reduction_result);
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> duration = end - start;
+    LOG(INFO) << reduction_id.ToString() << " is reduced using "
+              << duration.count();
+    print_reduction_result<float>(reduction_id, reduction_result, sum);
   }
-  DCHECK(object_size % sizeof(float) == 0);
-
-  ObjectID rank_object_id = object_ids[rank];
-  std::shared_ptr<Buffer> reduction_result;
-
-  put_random_buffer<float>(store, rank_object_id, object_size);
-  barrier(rank, redis_address, 7777, world_size, my_address);
-
-  auto start = std::chrono::system_clock::now();
-  if (rank == 0) {
-    store.Reduce(object_ids, reduction_id);
-  }
-  store.Get(reduction_id, &reduction_result);
-  auto end = std::chrono::system_clock::now();
-  std::chrono::duration<double> duration = end - start;
-  LOG(INFO) << reduction_id.ToString() << " is reduced using "
-            << duration.count();
-  print_reduction_result<float>(reduction_id, reduction_result, sum);
 
   exit_thread.join();
   store.join_tasks();
