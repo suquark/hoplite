15c4245cba81e1828a4c0a21928bac5858a43f62
Zhuohan Li <zhuohan123@vip.qq.com>
2020-08-26 16:58:43 -0700

    fix gather

--- gather_test.cc
+++ gather_test.cc
@@ -14,6 +14,7 @@ int main(int argc, char **argv) {
   int64_t world_size = std::strtoll(argv[3], NULL, 10);
   int64_t rank = std::strtoll(argv[4], NULL, 10);
   int64_t object_size = std::strtoll(argv[5], NULL, 10);
+  int64_t n_trials = std::strtoll(argv[6], NULL, 10);
 
   ::ray::RayLog::StartRayLog(my_address, ::ray::RayLogLevel::DEBUG);
 
@@ -25,37 +26,39 @@ int main(int argc, char **argv) {
 
   std::thread exit_thread(timed_exit, 20);
 
-  std::vector<ObjectID> object_ids;
-  float sum = 0;
-  for (int i = 0; i < world_size; i++) {
-    auto oid = object_id_from_integer(i);
-    object_ids.push_back(oid);
-    auto rnum = get_uniform_random_float(oid.Hex());
-    sum += rnum;
-  }
-  DCHECK(object_size % sizeof(float) == 0);
-
-  ObjectID rank_object_id = object_ids[rank];
-  std::unordered_map<ObjectID, std::shared_ptr<Buffer>> gather_result;
-
-  put_random_buffer<float>(store, rank_object_id, object_size);
-
-  barrier(rank, redis_address, 7777, world_size, my_address);
-
-  if (rank == 0) {
-    auto start = std::chrono::system_clock::now();
-    for (auto &object_id : object_ids) {
-      store.Get(object_id, &gather_result[object_id]);
+  for (int trial = 0; trial < n_trials; trial++) {
+    std::vector<ObjectID> object_ids;
+    float sum = 0;
+    for (int i = 0; i < world_size; i++) {
+      auto oid = object_id_from_integer(trial * 1000000 + i);
+      object_ids.push_back(oid);
+      auto rnum = get_uniform_random_float(oid.Hex());
+      sum += rnum;
     }
-    auto end = std::chrono::system_clock::now();
-    std::chrono::duration<double> duration = end - start;
-    LOG(INFO) << "gathered using " << duration.count() << " seconds";
+    DCHECK(object_size % sizeof(float) == 0);
 
-    uint32_t sum_crc = 0;
-    for (auto &object_id : object_ids) {
-      sum_crc += gather_result[object_id]->CRC32();
+    ObjectID rank_object_id = object_ids[rank];
+    std::unordered_map<ObjectID, std::shared_ptr<Buffer>> gather_result;
+
+    put_random_buffer<float>(store, rank_object_id, object_size);
+
+    barrier(redis_address, 7777, world_size);
+
+    if (rank == 0) {
+      auto start = std::chrono::system_clock::now();
+      for (auto &object_id : object_ids) {
+        store.Get(object_id, &gather_result[object_id]);
+      }
+      auto end = std::chrono::system_clock::now();
+      std::chrono::duration<double> duration = end - start;
+      LOG(INFO) << "gathered using " << duration.count() << " seconds";
+
+      uint32_t sum_crc = 0;
+      for (auto &object_id : object_ids) {
+        sum_crc += gather_result[object_id]->CRC32();
+      }
+      LOG(INFO) << "CRC32 for objects is " << sum_crc;
     }
-    LOG(INFO) << "CRC32 for objects is " << sum_crc;
   }
 
   exit_thread.join();
