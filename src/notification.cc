9f55e3d85df91b2aac242ba90d630e0b62ffaed1
Zhuohan Li <zhuohan123@vip.qq.com>
2020-08-27 01:06:40 -0700

    Update notification.cc

--- src/notification.cc
+++ src/notification.cc
@@ -123,7 +123,7 @@ NotificationServiceImpl::NotificationServiceImpl(
 grpc::Status NotificationServiceImpl::Barrier(grpc::ServerContext *context,
                                               const BarrierRequest *request,
                                               BarrierReply *reply) {
-  TIMELINE("Barrier Loop");
+  TIMELINE("Barrier");
   int n_nodes = request->num_of_nodes();
   barrier_arrive_counter_++;
   while (barrier_arrive_counter_ < n_nodes)
