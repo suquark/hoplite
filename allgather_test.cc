5406674ba42dd840ebdf70e7c454fcdaeb4372a7
Zhuohan Li <zhuohan123@vip.qq.com>
2020-08-26 17:01:50 -0700

    fix all_gather

--- allgather_test.cc
+++ allgather_test.cc
@@ -26,35 +26,38 @@ int main(int argc, char **argv) {
   // TODO: allgather could be very slow. we could even extend it longer.
   std::thread exit_thread(timed_exit, 40);
 
-  std::vector<ObjectID> object_ids;
-  float sum = 0;
-  for (int i = 0; i < world_size; i++) {
-    auto oid = object_id_from_integer(i);
-    object_ids.push_back(oid);
-    auto rnum = get_uniform_random_float(oid.Hex());
-    sum += rnum;
+  for (int trial = 0; trial < n_trials; trial++) {
+    std::vector<ObjectID> object_ids;
+    float sum = 0;
+    for (int i = 0; i < world_size; i++) {
+      auto oid = object_id_from_integer(trial * 1000000 + i);
+      object_ids.push_back(oid);
+      auto rnum = get_uniform_random_float(oid.Hex());
+      sum += rnum;
+    }
+    DCHECK(object_size % sizeof(float) == 0);
+
+    ObjectID rank_object_id = object_ids[rank];
+    std::unordered_map<ObjectID, std::shared_ptr<Buffer>> gather_result;
+
+    put_random_buffer<float>(store, rank_object_id, object_size);
+
+    barrier(redis_address, 7777, world_size);
+
+    auto start = std::chrono::system_clock::now();
+    for (auto &object_id : object_ids) {
+      store.Get(object_id, &gather_result[object_id]);
+    }
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> duration = end - start;
+    LOG(INFO) << "allgathered using " << duration.count() << " seconds";
+    uint32_t sum_crc = 0;
+    for (auto &object_id : object_ids) {
+      sum_crc += gather_result[object_id]->CRC32();
+    }
+    LOG(INFO) << "CRC32 for objects is " << sum_crc;
   }
-  DCHECK(object_size % sizeof(float) == 0);
 
-  ObjectID rank_object_id = object_ids[rank];
-  std::unordered_map<ObjectID, std::shared_ptr<Buffer>> gather_result;
-
-  put_random_buffer<float>(store, rank_object_id, object_size);
-
-  barrier(rank, redis_address, 7777, world_size, my_address);
-
-  auto start = std::chrono::system_clock::now();
-  for (auto &object_id : object_ids) {
-    store.Get(object_id, &gather_result[object_id]);
-  }
-  auto end = std::chrono::system_clock::now();
-  std::chrono::duration<double> duration = end - start;
-  LOG(INFO) << "allgathered using " << duration.count() << " seconds";
-  uint32_t sum_crc = 0;
-  for (auto &object_id : object_ids) {
-    sum_crc += gather_result[object_id]->CRC32();
-  }
-  LOG(INFO) << "CRC32 for objects is " << sum_crc;
   exit_thread.join();
   store.join_tasks();
   return 0;
