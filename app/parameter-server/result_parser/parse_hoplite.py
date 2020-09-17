7e0f4f993e306e06763f8f4f448eec29c0148b6c
Zhuohan Li <zhuohan123@vip.qq.com>
2020-09-16 20:50:03 -0700

    fix parse_hoplite

--- app/parameter-server/result_parser/parse_hoplite.py
+++ app/parameter-server/result_parser/parse_hoplite.py
@@ -6,6 +6,7 @@ filename = sys.argv[1]
 n_nodes = int(sys.argv[2])
 
 all_step_time = []
+min_len = 1e10
 
 for i in range(n_nodes):
     step_time_rank = []
@@ -14,9 +15,10 @@ for i in range(n_nodes):
             if f" {i} in actor time" in line:
                 step_time_rank.append(float(line.split(f" {i} in actor time")[1]))
     print(len(step_time_rank))
+    min_len = min(min_len, len(step_time_rank))
     all_step_time.append(np.array(step_time_rank))
 
-all_step_time = np.array(all_step_time)
+all_step_time = np.array([a[:min_len] for a in all_step_time])
 all_step_time = all_step_time[:, 5:]
 all_step_time = np.amax(all_step_time, axis=0)
 
