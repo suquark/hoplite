03e182dbc139b077cd95879bbbe6dd7f73585a6e
Zhuohan Li <zhuohan123@vip.qq.com>
2020-10-07 01:04:50 +0000

    fix bugs

--- app/parameter-server/result_parser/parse_async_ps_hoplite.py
+++ app/parameter-server/result_parser/parse_async_ps_hoplite.py
@@ -12,7 +12,7 @@ with open(filename, 'r') as f:
         if f"step time:" in line:
             all_step_time.append(float(line.split(f"step time:")[1]))
 
-all_step_time = all_step_time[6:]
+all_step_time = np.array(all_step_time[6:])
 all_step_throughput = 1.0 / all_step_time
-all_step_throughput = (all_step_throughput[0::1] + all_step_throughput[1::1]) / 2
+all_step_throughput = (all_step_throughput[0::2] + all_step_throughput[1::2]) / 2
 print(np.mean(all_step_throughput), np.std(all_step_throughput))
--- app/parameter-server/result_parser/parse_ray.py
+++ app/parameter-server/result_parser/parse_ray.py
@@ -12,6 +12,6 @@ with open(filename, 'r') as f:
         if f"step time:" in line:
             all_step_time.append(float(line.split(f"step time:")[1]))
 
-all_step_time = all_step_time[3:]
+all_step_time = np.array(all_step_time[3:])
 all_step_throughput = 1.0 / all_step_time
 print(np.mean(all_step_throughput), np.std(all_step_throughput))
