db5a539752697dc065f452d5a9c7fe78e14411bd
Siyuan <suquark@gmail.com>
2021-01-11 00:25:09 -0800

    hoplite version

--- app/ray_serve/hoplite_model_ensembling.py
+++ app/ray_serve/hoplite_model_ensembling.py
@@ -1,3 +1,4 @@
+import argparse
 import time
 
 import ray
@@ -9,13 +10,20 @@ import requests
 import torch
 import torchvision.models as models
 
+import py_distributed_object_store as store_lib
+import utils
+
 
 @ray.remote(num_gpus=1)
 class ModelWorker:
-    def __init__(self, model_name):
+    def __init__(self, model_name, args_dict):
         self.model = getattr(models, model_name)().cuda().eval()
+        self.store = utils.create_store_using_dict(args_dict)
 
-    def inference(self, x):
+    def inference(self, x_id):
+        buffer = self.store.get(x_id)
+        x = np.frombuffer(buffer, dtype=np.int32)
+
         x = torch.from_numpy(x).cuda()
         with torch.no_grad():
             # with torch.cuda.amp.autocast():
@@ -30,7 +38,9 @@ served_models = (
 
 
 class InferenceHost:
-    def __init__(self):
+    def __init__(self, args_dict):
+        self.store = utils.create_store_using_dict(args_dict)
+
         # Imagine this is a data labeling task, and the user have loaded a bunch of images,
         # cached in memory. Because it is interactive,
         # 1) The user can choose any set of images.
@@ -44,18 +54,31 @@ class InferenceHost:
         # VGG16 is super slow compared to other models.
         for model_name in served_models:
             # TODO: distribute workers to different nodes.
-            self.models.append(ModelWorker.remote(model_name))
+            self.models.append(ModelWorker.remote(model_name, args_dict))
+        self.request_id = 0
 
     def __call__(self, request):
         # convert torch tensor to numpy speeds up Ray.
         # The original serialization of pytorch tensor would be way too slow.
         x = self.images.numpy()
-        results = ray.get([m.inference.remote(x) for m in self.models])
+
+        object_id = utils.object_id_from_int(self.request_id)
+        buffer = store_lib.Buffer.from_buffer(x)
+        self.store.put(buffer, object_id)
+
+        results = ray.get([m.inference.remote(object_id) for m in self.models])
         cls = np.argmax(sum(results), 1)
         return str(cls.tolist())
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='ray serve with hoplite')
+    # We just use this to extract default configurations
+    utils.add_arguments(parser)
+    args = parser.parse_args()
+    args_dict = utils.extract_dict_from_args(args)
+    utils.start_location_server()
+
     ray.init(address='auto')
     client = serve.start()
     for ep in client.list_endpoints().keys():
@@ -81,5 +104,4 @@ if __name__ == "__main__":
         durations.append(1/(time.time() - start))
 
     print(f"{np.mean(durations):.6f} ± {np.std(durations):.6f} requests/s")
-    # > {"count": 1}
     print(ray.get(client.get_handle("ray_endpoint").remote()))
