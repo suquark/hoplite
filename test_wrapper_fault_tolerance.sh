ad6ece15de7e2ea72f31cb4a677701305ec4d64b
Siyuan <suquark@gmail.com>
2021-01-20 03:24:20 -0800

    fix

--- test_wrapper_fault_tolerance.sh
+++ test_wrapper_fault_tolerance.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
+trap 'echo delaying MPI shutdown...' INT TERM
 logging_file=$HOPLITE_LOGGING_DIR/rank_$OMPI_COMM_WORLD_RANK.log
-( $@ 2>&1 & echo $! > /tmp/hoplite_test.pid ) | tee $logging_file &
-wait
-sleep 10  # wait for fault-tolerance
+$@ 2>&1 | tee $logging_file
+sleep 10
