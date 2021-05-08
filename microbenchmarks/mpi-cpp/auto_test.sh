44c658529763cc81c6f7b5494b0da671beae664b
Siyuan <suquark@gmail.com>
2021-05-07 17:11:22 -0700

    repeat 5 times

--- microbenchmarks/mpi-cpp/auto_test.sh
+++ microbenchmarks/mpi-cpp/auto_test.sh
@@ -3,7 +3,7 @@
 for test_name in multicast reduce gather allreduce; do
   for num_nodes in 4 8 12 16; do
     for sz in 10 15 20 25 30; do
-        for i in `seq 10`; do
+        for i in `seq 5`; do
           obj_size=$((2**$sz))
           ./run_test.sh ${test_name} $num_nodes $obj_size
         done
