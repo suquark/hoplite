6ea014d8ce2e42e897d1d3d0e8cdc4eba08ad9ef
Siyuan <suquark@gmail.com>
2021-05-16 17:49:06 -0700

    update

--- python/setup.sh
+++ python/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# make hoplite importable
 script_dir=$(dirname "${BASH_SOURCE[0]}")
 site_packages=$(python -c 'import site; print(site.getsitepackages()[0])')
 echo $(realpath $script_dir) > $site_packages/easy-install.pth
