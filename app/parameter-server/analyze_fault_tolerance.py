067d7dfad44b6dce119c1f70f890039d7f66478b
Siyuan <suquark@gmail.com>
2021-05-25 01:12:15 -0700

    update script

--- app/parameter-server/analyze_fault_tolerance.py
+++ app/parameter-server/analyze_fault_tolerance.py
@@ -4,9 +4,11 @@ import numpy as np
 with open('ray_asgd_fault_tolerance.json', 'r') as f:
     ray_log = json.load(f)
     durations = [l['duration'] for l in ray_log if l['event'] == 'fail']
-    print(f"Baseline latency caused by failure: {np.mean(durations):.6f} ± {np.std(durations):.6f}s")
+    # we only fail once in the paper, so no calculating std here
+    print(f"Baseline latency caused by failure: {np.mean(durations):.6f}s")
 
 with open('hoplite_asgd_fault_tolerance.json', 'r') as f:
     hoplite_log = json.load(f)
     durations = [l['duration'] for l in hoplite_log if l['event'] == 'fail']
-    print(f"Hoplite latency caused by failure: {np.mean(durations):.6f} ± {np.std(durations):.6f}s")
+    # we only fail once in the paper, so no calculating std here
+    print(f"Hoplite latency caused by failure: {np.mean(durations):.6f}s")
