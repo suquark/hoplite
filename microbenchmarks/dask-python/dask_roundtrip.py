e8516dbf81ab25f704cbd4466c8d7916d8a08d15
Siyuan <suquark@gmail.com>
2021-05-25 21:24:37 -0700

    fix

--- microbenchmarks/dask-python/dask_roundtrip.py
+++ microbenchmarks/dask-python/dask_roundtrip.py
@@ -37,7 +37,7 @@ def main():
     # print(duration)
     with open(f"dask-roundtrip.csv", "w") as f:
         f.write("1 KB, 1 MB, 1 GB, 1 KB (std), 1 MB (std), 1 GB (std)\n"
-                ",".join(means + stds))
+                ",".join(map(str, means + stds)))
 
 
 if __name__ == "__main__":
